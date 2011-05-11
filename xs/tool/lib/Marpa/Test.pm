2d3b90e82cd3bcb400319eb4e1a3f560c1b1a4ff
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-05-11 11:35:41 -0700

    Refactoring namespace

---
+++ xs/tool/lib/Marpa/Test.pm
@@ -0,0 +1,44 @@
+# Copyright 2011 Jeffrey Kegler
+# This file is part of Marpa::PP.  Marpa::PP is free software: you can
+# redistribute it and/or modify it under the terms of the GNU Lesser
+# General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Marpa::PP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser
+# General Public License along with Marpa::PP.  If not, see
+# http://www.gnu.org/licenses/.
+
+package Marpa::PP::Test;
+
+use 5.010;
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+Marpa::exception('Test::More not loaded')
+    if not defined &Test::More::is;
+
+BEGIN {
+    ## no critic (BuiltinFunctions::ProhibitStringyEval)
+    ## no critic (ErrorHandling::RequireCheckingReturnValueOfEval)
+    eval 'use Test::Differences';
+    ## use critic
+} ## end BEGIN
+
+## no critic (Subroutines::RequireArgUnpacking)
+sub Marpa::Test::is {
+## use critic
+    goto &Test::Differences::eq_or_diff
+        if defined &Test::Differences::eq_or_diff && @_ > 1;
+    @_ = map { ref $_ ? Data::Dumper::Dumper(@_) : $_ } @_;
+    goto &Test::More::is;
+} ## end sub Marpa::Test::is
+
+1;
+
