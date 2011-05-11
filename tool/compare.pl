e5d0a9f3a1a90c07cb3272acb6511cc3c9e1c194
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-05-11 11:33:24 -0700

    Refactoring namespace

--- tool/compare.pl
+++ tool/compare.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use File::Spec;
 use File::Compare;
+use File::Find;
 
 my @t_files = ();
 push @t_files, glob "pp/t/shared/*.t";
@@ -17,25 +18,35 @@ for my $t_file (@t_files) {
 }
 my $eqx = 'xs/t/shared/equation.t';
 my $eqp = 'pp/t/shared/equation.t';
-for my $t_file (keys %t_file) {
-     for my $file (map { $_ . $t_file } qw(
-	 pp/t/shared/ pp/t/shared/common/
-	 xs/t/shared/ xs/t/shared/common/ )) {
-	 if (! -f $file) {
-	      say STDERR "Missing $file";
-	 }
-	 my $pp = "pp/t/shared/common/$t_file";
-	 my $xs = "xs/t/shared/common/$t_file";
-	 if (File::Compare::compare($pp, $xs)) {
-	      say STDERR "Different: $xs vs $pp";
-	 }
-	 $pp = "pp/t/shared/$t_file";
-	 if (File::Compare::compare($pp, $eqp)) {
-	      say STDERR "Different: $eqp vs $pp";
-	 }
-	 $xs = "xs/t/shared/$t_file";
-	 if (File::Compare::compare($xs, $eqx)) {
-	      say STDERR "Different: $eqx vs $xs";
-	 }
-     }
+for my $t_file ( keys %t_file ) {
+    for my $file (
+        map { $_ . $t_file }
+        qw(
+        pp/t/shared/ pp/t/shared/common/
+        xs/t/shared/ xs/t/shared/common/ )
+        )
+    {
+        if ( !-f $file ) {
+            say STDERR "Missing $file";
+        }
+    } ## end for my $file ( map { $_ . $t_file } qw(...))
+    my $pp = "pp/t/shared/common/$t_file";
+    my $xs = "xs/t/shared/common/$t_file";
+    if ( File::Compare::compare( $pp, $xs ) ) {
+        say STDERR "Different: $xs vs $pp";
+    }
+    $pp = "pp/t/shared/$t_file";
+    if ( File::Compare::compare( $pp, $eqp ) ) {
+        say STDERR "Different: $eqp vs $pp";
+    }
+    $xs = "xs/t/shared/$t_file";
+    if ( File::Compare::compare( $xs, $eqx ) ) {
+        say STDERR "Different: $eqx vs $xs";
+    }
+} ## end for my $t_file ( keys %t_file )
+my @found = ();
+sub wanted {
+     -d or push @found, $File::Find::name;
 }
+File::Find::find(\&wanted, 'pp/tool');
+say join "\n", @found;
