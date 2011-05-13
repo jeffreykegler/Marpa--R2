31440ce12bcd0b8819c061994eb6a2d53127ba14
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-05-13 08:44:07 -0700

    Fix Doc name.  CPAN PP-0.005_006

--- pp/lib/Marpa/PP.pm
+++ pp/lib/Marpa/PP.pm
@@ -19,7 +19,7 @@ use 5.010;
 use strict;
 use warnings;
 use vars qw($VERSION $STRING_VERSION);
-$VERSION = '0.005_005';
+$VERSION = '0.005_006';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
