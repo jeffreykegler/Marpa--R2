9bb341e17fb0a228bb399259c4f0bfb92d2288e0
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-06-09 08:24:32 -0700

    Converting bocage to C.  t+ html+

--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA);
-$VERSION = '0.005_015';
+$VERSION = '0.005_016';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
