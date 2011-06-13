08034593cf58cd60a5e16b562db75cd9a3b3e04c
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-06-12 18:27:36 -0700

    XS 0.005_018 less MARPA_ASSERT

--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA);
-$VERSION = '0.005_017';
+$VERSION = '0.005_018';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
