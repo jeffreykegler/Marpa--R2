ae8f80af463df1869ee93b22294cf66ba22a70af
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-07-21 20:50:55 -0700

    Just uploaded Marpa-XS-0.005_034.tar.gz.

--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION = '0.005_033';
+$VERSION = '0.005_034';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 $DEBUG = 0;
