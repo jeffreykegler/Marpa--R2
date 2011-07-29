6142ead8cc129e42d9672f1d276ae057d62c60cb
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-07-28 21:25:05 -0700

    Cleanup after bocage converstion.  t+

--- xs/lib/Marpa/XS/Value.pm
+++ xs/lib/Marpa/XS/Value.pm
@@ -1557,9 +1557,12 @@ sub Marpa::XS::Recognizer::value {
 
 	$#{$or_nodes} = -1;
 	OR_NODE: for ( my $or_node_id = 0; ; $or_node_id++ ) {
-	    my ( $origin, $set, $rule_id, $position, $first_and_id, $and_count ) =
+	    my ( undef, undef, undef, undef, $first_and_id, $and_count ) =
 		$recce_c->or_node( $or_node_id );
+	    my $origin = $recce_c->or_node_origin( $or_node_id );
 	    last OR_NODE if not defined $origin;
+	    my $set = $recce_c->or_node_set( $or_node_id );
+	    my $rule_id = $recce_c->or_node_rule( $or_node_id );
 
 	    my $or_node = [];
 	    $or_node->[Marpa::XS::Internal::Or_Node::ID]  = $or_node_id;
