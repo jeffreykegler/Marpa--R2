de54e27c5accd15d9239030d2994b2b8d4738dfb
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-07-28 22:37:28 -0700

    Undo erroneous packaging "fix"

--- pp/pperl/Marpa/Perl/Installed.pm
+++
@@ -1,9 +0,0 @@
-# This file is written by Build.PL
-# It is not intended to be modified directly
-
-package pperl::Marpa::Perl::Installed;
-use vars qw($VERSION $STRING_VERSION);
-$VERSION = '0.008000';
-$STRING_VERSION = $VERSION;
-$VERSION = eval $VERSION;
-1;
--- xs/pperl/Marpa/Perl/Installed.pm
+++
@@ -1,9 +0,0 @@
-# This file is written by Build.PL
-# It is not intended to be modified directly
-
-package pperl::Marpa::Perl::Installed;
-use vars qw($VERSION $STRING_VERSION);
-$VERSION = '0.005_035';
-$STRING_VERSION = $VERSION;
-$VERSION = eval $VERSION;
-1;
