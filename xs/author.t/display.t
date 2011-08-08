c08b22b73c7c01c023da728eaf58b5836a7d192f
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-08-08 10:59:18 -0700

    Packaging

---
+++ xs/author.t/display.t
@@ -0,0 +1,262 @@
+#!perl
+# Copyright 2011 Jeffrey Kegler
+# This file is part of Marpa::XS.  Marpa::XS is free software: you can
+# redistribute it and/or modify it under the terms of the GNU Lesser
+# General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Marpa::XS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser
+# General Public License along with Marpa::XS.  If not, see
+# http://www.gnu.org/licenses/.
+
+use 5.010;
+use strict;
+use warnings;
+
+use English qw( -no_match_vars );
+use Fatal qw(open close);
+use Text::Diff;
+use Getopt::Long qw(GetOptions);
+use List::Util;
+use Test::More 0.94;
+use Carp;
+
+use Perl::Tidy;
+use Text::Wrap;
+
+use lib 'tool/lib';
+use Marpa::Display;
+
+my $warnings = 0;
+my $options_result = GetOptions( 'warnings' => \$warnings );
+
+Marpa::exception("$PROGRAM_NAME options parsing failed")
+    if not $options_result;
+
+my %exclude = map { ( $_, 1 ) } qw(
+    Makefile.PL
+);
+
+my @additional_files = qw(
+);
+
+my @test_files = @ARGV;
+my $debug_mode = scalar @test_files;
+if ( not $debug_mode ) {
+
+    for my $additional_file (@additional_files) {
+        Test::More::diag("Adding $additional_file");
+        push @test_files, $additional_file;
+    }
+
+    open my $manifest, '<', 'MANIFEST'
+        or Marpa::exception("Cannot open MANIFEST: $ERRNO");
+    FILE: while ( my $file = <$manifest> ) {
+        chomp $file;
+        $file =~ s/\s*[#].*\z//xms;
+	next FILE if $file =~ m( [/] old_pod [/] )xms;
+        my ($ext) = $file =~ / [.] ([^.]+) \z /xms;
+        next FILE if not defined $ext;
+        $ext = lc $ext;
+        next FILE
+            if $ext ne 'pod'
+                and $ext ne 'pl'
+                and $ext ne 'pm'
+                and $ext ne 't';
+        push @test_files, $file;
+    }    # FILE
+    close $manifest;
+
+    my %file_seen = ();
+    FILE: for my $test_file (@test_files) {
+
+        next FILE if $exclude{$test_file};
+        next FILE if -d $test_file;
+        if ( $file_seen{$test_file}++ ) {
+            Test::More::diag("Duplicate file: $test_file");
+        }
+    } ## end for my $test_file (@test_files)
+    @test_files = keys %file_seen;
+
+} ## end if ( not $debug_mode )
+
+my $error_file;
+## no critic (InputOutput::RequireBriefOpen)
+if ($debug_mode) {
+    open $error_file, '>&STDOUT'
+        or Marpa::exception("Cannot dup STDOUT: $ERRNO");
+}
+else {
+    open $error_file, '>', 'author.t/display.errs'
+        or Marpa::exception("Cannot open display.errs: $ERRNO");
+}
+## use critic
+
+my $display_data = Marpa::XS::Display->new();
+
+FILE: for my $file (@test_files) {
+    if ( not -f $file ) {
+        Test::More::fail(qq{"$file" is not a file});
+        next FILE;
+    }
+    $display_data->read($file);
+
+} ## end for my $file (@test_files)
+
+my @formatting_instructions = qw(perltidy
+    remove-display-indent
+    remove-blank-last-line inline
+    partial flatten normalize-whitespace);
+
+sub format_display {
+    my ( $text, $instructions, $is_copy ) = @_;
+    my $result = ${$text};
+
+    if ( $instructions->{'remove-display-indent'} and $is_copy ) {
+        my ($first_line_spaces) = ( $result =~ /^ (\s+) \S/xms );
+        $first_line_spaces = quotemeta $first_line_spaces;
+        $result =~ s/^$first_line_spaces//gxms;
+    }
+    if ( $instructions->{'inline'} ) {
+        my $min_indent = 99_999_999;
+        my @text = grep {/ [^ ] /xms} split /\n/xms, $result;
+        for my $line (@text) {
+            my ($s) = ( $line =~ / \A  ([ ]* ) /xms );
+            my $indent = length $s;
+            $min_indent > $indent and $min_indent = $indent;
+        }
+        $result = join "\n", map { substr $_, $min_indent } @text;
+        my $tidied;
+
+        # perltidy options chosen to make it as likely
+        # as possible that code which differs
+        # only in whitespace
+        # will end up the same.
+        Perl::Tidy::perltidy(
+            source      => \$result,
+            destination => \$tidied,
+            perltidyrc  => \'-sbt=0 -iob -dcsc -sil=0',
+        );
+        $result = $tidied;
+    } ## end if ( $instructions->{'inline'} )
+    if ( $instructions->{'remove-blank-last-line'} ) {
+        $result =~ s/^[ \t]*\n\z//xms;
+    }
+    if ( $instructions->{'flatten'} ) {
+        $result =~ s/[\n\r]/ /gxms;
+    }
+    if ( $instructions->{'normalize-whitespace'} ) {
+        $result =~ s/^\s+//gxms;
+        $result =~ s/\s+$//gxms;
+        $result =~ s/[ \f\t]+/ /gxms;
+        $result =~ s/\n+/\n/gxms;
+    } ## end if ( $instructions->{'normalize-whitespace'} )
+    if ( defined( my $tidy_options = $instructions->{'perltidy'} ) ) {
+        my $tidied;
+        Perl::Tidy::perltidy(
+            source      => \$result,
+            destination => \$tidied,
+            perltidyrc  => \$tidy_options
+        );
+        $result = $tidied;
+    } ## end if ( defined( my $tidy_options = $instructions->{'perltidy'...}))
+    return \$result;
+} ## end sub format_display
+
+# reformat two display according to the instructions in the
+# second, and compare.
+sub compare {
+    my ( $original, $copy ) = @_;
+    my $formatted_original =
+        format_display( \$original->{content}, $copy, 0 );
+    my $formatted_copy = format_display( \$copy->{content}, $copy, 1 );
+    if ( $copy->{partial} ) {
+        return 1 if -1 != index ${$formatted_original}, ${$formatted_copy};
+        Test::More::diag(
+            $copy->{filename},
+            ' vs. ',
+            $original->{filename},
+            "\n",
+            "Sought Substring:\n",
+            Text::Wrap::wrap( q{    }, q{    }, ${$formatted_copy} ),
+            "\nOriginal:\n",
+            Text::Wrap::wrap( q{    }, q{    }, ${$formatted_original} )
+        );
+        return 0;
+    } ## end if ( $copy->{partial} )
+    return 1 if ${$formatted_original} eq ${$formatted_copy};
+    Test::More::diag(
+        'Differences: ',
+        $original->{filename},
+        ' vs. ',
+        $copy->{filename},
+        "\n",
+        (   Text::Diff::diff $formatted_original,
+            $formatted_copy,
+            { STYLE => 'Table' }
+        )
+    );
+    return 0;
+} ## end sub compare
+
+my $tests_run        = 0;
+my $displays_by_name = $display_data->{displays};
+DISPLAY_NAME: for my $display_name ( keys %{$displays_by_name} ) {
+
+    my $displays = $displays_by_name->{$display_name};
+    if ( scalar @{$displays} <= 1 ) {
+        Test::More::fail(
+            qq{Display "$display_name" has only one instance, in file }
+                . $displays->[0]->{filename} );
+        $tests_run++;
+    } ## end if ( scalar @{$displays} <= 1 )
+
+    # find the "original"
+    my $original_ix;
+    DISPLAY: for my $display_ix ( 0 .. $#{$displays} ) {
+        if (not grep { $_ ~~ \@formatting_instructions }
+            keys %{ $displays->[$display_ix] }
+            )
+        {
+            $original_ix = $display_ix;
+        } ## end if ( not grep { $_ ~~ \@formatting_instructions } keys...)
+    } ## end for my $display_ix ( 0 .. $#{$displays} )
+
+    # Warn if there wasn't a clear original?
+    $original_ix //= 0;    # default to the first
+
+    DISPLAY: for my $copy_ix ( 0 .. $#{$displays} ) {
+        next DISPLAY if $copy_ix == $original_ix;
+        Test::More::ok compare( $displays->[$original_ix],
+            $displays->[$copy_ix] ), "$display_name, copy $copy_ix";
+        $tests_run++;
+    } ## end for my $copy_ix ( 0 .. $#{$displays} )
+
+} ## end for my $display_name ( keys %{$displays_by_name} )
+
+my $verbatim_by_file = $display_data->{verbatim_lines};
+VERBATIM_FILE: for my $verbatim_file ( keys %{$verbatim_by_file} ) {
+    my @unchecked      = ();
+    my $verbatim_lines = $verbatim_by_file->{$verbatim_file};
+    for my $verbatim_line_number ( 1 .. $#{$verbatim_lines} ) {
+        my $verbatim_line = $verbatim_lines->[$verbatim_line_number];
+        if ($verbatim_line) {
+            push @unchecked, "$verbatim_line_number: $verbatim_line";
+        }
+    } ## end for my $verbatim_line_number ( 1 .. $#{$verbatim_lines...})
+    next VERBATIM_FILE if not @unchecked;
+    Test::More::fail( qq{Verbatim line(s) not checked in "$verbatim_file": }
+            . ( scalar @unchecked )
+            . " lines\n"
+            . ( join "\n", @unchecked ) );
+    $tests_run++;
+} ## end for my $verbatim_file ( keys %{$verbatim_by_file} )
+
+Test::More::done_testing($tests_run);
+
+__END__
---
+++ xs/tool/lib/Marpa/Display.pm
@@ -0,0 +1,271 @@
+# Copyright 2011 Jeffrey Kegler
+# This file is part of Marpa::XS.  Marpa::XS is free software: you can
+# redistribute it and/or modify it under the terms of the GNU Lesser
+# General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Marpa::XS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser
+# General Public License along with Marpa::XS.  If not, see
+# http://www.gnu.org/licenses/.
+
+package Marpa::XS::Display;
+
+use 5.010;
+use strict;
+use warnings;
+use integer;
+use Fatal qw(open close);
+use YAML::XS;
+use Data::Dumper;    # for debugging
+use Carp;
+
+package Marpa::XS::Display::Internal;
+
+use English qw( -no_match_vars );
+
+sub Marpa::XS::Display::new {
+    my ($class) = @_;
+    my $self = {};
+    $self->{displays}         = {};
+    $self->{ignored_displays} = [];
+    return bless $self, $class;
+} ## end sub Marpa::XS::Display::new
+
+@Marpa::XS::Display::Internal::DISPLAY_SPECS = qw(
+    start-after-line end-before-line perltidy normalize-whitespace name
+    remove-display-indent
+    remove-blank-last-line
+    partial flatten inline
+    ignore
+);
+
+sub Marpa::XS::Display::read {
+    my ( $self, $data_arg, $file_name ) = @_;
+    my @lines;
+    GET_LINES: {
+        if ( not ref $data_arg ) {
+            $file_name //= $data_arg;
+            open my $fh, q{<}, $data_arg;
+            @lines = <$fh>;
+            close $fh;
+            last GET_LINES;
+        } ## end if ( not ref $data_arg )
+        $file_name //= q{?};
+        @lines = split /\n/xms, ${$data_arg};
+    } ## end GET_LINES:
+
+    chomp @lines;
+    my @verbatim_lines;
+    my $in_pod      = 0;
+    my $in_verbatim = 0;
+    my $in_begin;
+    POD_LINE: for my $zero_based_line ( 0 .. $#lines ) {
+        my $line = $lines[$zero_based_line];
+        if ( $in_pod and $line =~ /\A=cut/xms ) {
+            $in_pod      = 0;
+            $in_verbatim = 0;
+            $in_begin    = undef;
+            next POD_LINE;
+        } ## end if ( $in_pod and $line =~ /\A=cut/xms )
+        if ( not $in_pod and $line =~ /\A=[a-zA-Z]/xms ) {
+            $in_pod = 1;
+        }
+        next POD_LINE if not $in_pod;
+
+        # at this point out $in_pod indicates if we are
+        # in a pod sequence
+        if ( $in_pod and not $in_begin and $line =~ /\A=begin\s+(.*)/xms ) {
+            my $begin_identifier = $1;
+            if ( $begin_identifier !~ /\A:/xms ) {
+                $in_begin = $begin_identifier;
+            }
+        } ## end if ( $in_pod and not $in_begin and $line =~ ...)
+        if ( $in_begin and $line =~ /\A=end\s+(.*)/xms ) {
+            my $begin_identifier = $1;
+            if ( $begin_identifier eq $in_begin ) {
+                $in_begin = undef;
+            }
+        } ## end if ( $in_begin and $line =~ /\A=end\s+(.*)/xms )
+
+        # Don't look for verbatim paragraph inside begin
+        next POD_LINE if $in_begin;
+
+        # Is this the start of a verbatim paragraph?
+        if ( not $in_verbatim and $line =~ /\A[ \t]/xms ) {
+            $in_verbatim = 1;
+        }
+        if ( $in_verbatim and $line =~ /\A[ \t]*\z/xms ) {
+            $in_verbatim = 0;
+        }
+        if ($in_verbatim) {
+            $verbatim_lines[ $zero_based_line + 1 ] = $line;
+        }
+    } ## end for my $zero_based_line ( 0 .. $#lines )
+    LINE: for my $zero_based_line ( 0 .. $#lines ) {
+        my $line = $lines[$zero_based_line];
+
+        my $display_spec;
+        my $display_spec_line_number = $zero_based_line + 1;
+        if ( $line =~ /^[#] \s+ Marpa::XS[:][:]Display/xms ) {
+
+            my $yaml = q{};
+            while ( ( my $yaml_line = $lines[ ++$zero_based_line ] )
+                =~ /^[#]/xms )
+            {
+                $yaml .= "$yaml_line\n";
+            }
+            if ( $yaml =~ / \S /xms ) {
+                $yaml =~ s/^ [#] \s? //xmsg;
+                local $main::EVAL_ERROR = undef;
+                my $eval_ok =
+                    eval { $display_spec = YAML::XS::Load($yaml); 1 };
+                if ( not $eval_ok ) {
+                    say {*STDERR} $main::EVAL_ERROR
+                        or Carp::croak("Cannot print: $ERRNO");
+                    say {*STDERR}
+                        "Fatal error in YAML Display spec at $file_name, line "
+                        . ( $display_spec_line_number + 1 )
+                        or Carp::croak("Cannot print: $ERRNO");
+                } ## end if ( not $eval_ok )
+            } ## end if ( $yaml =~ / \S /xms )
+        } ## end if ( $line =~ /^[#] \s+ Marpa::XS[:][:]Display/xms )
+
+        if ( $line =~ /^[=]for \s+ Marpa::XS[:][:]Display/xms ) {
+
+            my $yaml = q{};
+            while (
+                ( my $yaml_line = $lines[ ++$zero_based_line ] ) =~ /\S/xms )
+            {
+                $yaml .= "$yaml_line\n";
+            }
+            if ( $yaml =~ / \S /xms ) {
+                local $main::EVAL_ERROR = undef;
+                my $eval_ok =
+                    eval { $display_spec = YAML::XS::Load($yaml); 1 };
+                if ( not $eval_ok ) {
+                    say {*STDERR} $main::EVAL_ERROR
+                        or Carp::croak("Cannot print: $ERRNO");
+                    say {*STDERR}
+                        "Fatal error in YAML Display spec at $file_name, line "
+                        . ( $display_spec_line_number + 1 )
+                        or Carp::croak("Cannot print: $ERRNO");
+                } ## end if ( not $eval_ok )
+            } ## end if ( $yaml =~ / \S /xms )
+        } ## end if ( $line =~ /^[=]for \s+ Marpa::XS[:][:]Display/xms)
+
+        next LINE if not defined $display_spec;
+
+        SPEC: for my $spec ( keys %{$display_spec} ) {
+            next SPEC
+                if $spec ~~ \@Marpa::XS::Display::Internal::DISPLAY_SPECS;
+            say {*STDERR}
+                qq{Warning: Unknown display spec "$spec" in $file_name, line $display_spec_line_number}
+                or Carp::croak("Cannot print: $ERRNO");
+        } ## end for my $spec ( keys %{$display_spec} )
+
+        my $content;
+        my $content_start_line;
+        my $content_end_line;
+        if ( defined( my $end_pattern = $display_spec->{'end-before-line'} ) )
+        {
+            my $end_pat = qr/$end_pattern/xms;
+            if (defined(
+                    my $start_pattern = $display_spec->{'start-after-line'}
+                )
+                )
+            {
+                my $start_pat = qr/$start_pattern/xms;
+                PRE_CONTENT_LINE: while (1) {
+                    my $pre_content_line = $lines[ ++$zero_based_line ];
+                    if ( not defined $pre_content_line ) {
+                        say {*STDERR}
+                            qq{Warning: Pattern "$start_pattern" never found, },
+                            qq{started looking at $file_name, line $display_spec_line_number}
+                            or Carp::croak("Cannot print: $ERRNO");
+                        return $self;
+                    } ## end if ( not defined $pre_content_line )
+                    last PRE_CONTENT_LINE
+                        if $pre_content_line =~ /$start_pat/xms;
+                } ## end while (1)
+            } ## end if ( defined( my $start_pattern = $display_spec->{...}))
+
+            CONTENT_LINE: while (1) {
+                my $content_line = $lines[ ++$zero_based_line ];
+                if ( not defined $content_line ) {
+                    say {*STDERR}
+                        qq{Warning: Pattern "$end_pattern" never found, },
+                        qq{started looking at $file_name, line $display_spec_line_number}
+                        or Carp::croak("Cannot print: $ERRNO");
+                } ## end if ( not defined $content_line )
+                last CONTENT_LINE if $content_line =~ /$end_pat/xms;
+                $content .= "$content_line\n";
+                $content_end_line = $zero_based_line + 1;
+                $content_start_line //= $zero_based_line + 1;
+            } ## end while (1)
+        } ## end if ( defined( my $end_pattern = $display_spec->{...}))
+
+        if ( not defined $content ) {
+            CONTENT_LINE: while (1) {
+                my $content_line = $lines[ ++$zero_based_line ];
+                if ( not defined $content_line ) {
+                    say {*STDERR}
+                        q{Warning: Pattern "Marpa::XS::Display::End" never found,}
+                        . qq{started looking at $file_name, line $display_spec_line_number}
+                        or Carp::croak("Cannot print: $ERRNO");
+                    return $self;
+                } ## end if ( not defined $content_line )
+                last CONTENT_LINE
+                    if $content_line
+                        =~ /^[=]for \s+ Marpa::XS[:][:]Display[:][:]End\b/xms;
+                last CONTENT_LINE
+                    if $content_line
+                        =~ /^[#] \s* Marpa::XS[:][:]Display[:][:]End\b/xms;
+                $content .= "$content_line\n";
+                $content_end_line = $zero_based_line + 1;
+                $content_start_line //= $zero_based_line + 1;
+            } ## end while (1)
+        } ## end if ( not defined $content )
+
+        $content //= '!?! No Content Found !?!';
+
+        my $display_spec_name = $display_spec->{name};
+        my $ignore            = $display_spec->{ignore};
+        if ( not $display_spec_name and not $ignore ) {
+            say {*STDERR} q{Warning: Unnamed display }
+                . qq{at $file_name, line $display_spec_line_number}
+                or Carp::croak("Cannot print: $ERRNO");
+            next LINE;
+        } ## end if ( not $display_spec_name and not $ignore )
+
+        $display_spec->{filename}           = $file_name;
+        $display_spec->{display_spec_line}  = $display_spec_line_number;
+        $display_spec->{content}            = $content;
+        $display_spec->{content_start_line} = $content_start_line;
+        $display_spec->{content_end_line}   = $content_end_line;
+        $display_spec->{line}               = $content_start_line
+            // $display_spec_line_number;
+
+        $verbatim_lines[$_] = undef
+            for $display_spec->{line} .. $display_spec->{content_end_line};
+
+        if ( not $ignore ) {
+            push @{ $self->{displays}->{$display_spec_name} }, $display_spec;
+            next LINE;
+        }
+
+        push @{ $self->{ignored_displays} }, $display_spec;
+
+    } ## end for my $zero_based_line ( 0 .. $#lines )
+
+    $self->{verbatim_lines}->{$file_name} = \@verbatim_lines;
+
+    return $self;
+
+} ## end sub Marpa::XS::Display::read
+
+1;
