b4e8ea2f0594ca71193bf9d4e0ab56dd6b33dfd1
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-08-13 14:36:39 -0700

    Release XS 0.010000

--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION = '0.009_003';
+$VERSION = '0.010000';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 $DEBUG = 0;
