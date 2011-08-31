601e8572bd948882a596ef6933238b9b346c0a48
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-08-30 18:38:26 -0700

    Uploaded CPAN XS-0.013_000

--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION = '0.012000';
+$VERSION = '0.013_000';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 $DEBUG = 0;
