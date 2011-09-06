fd365bfa0ca02cd144a5a2008fa0e421334d1e8b
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-09-05 21:26:35 -0700

    Uploaded XS-0.013_003

--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION = '0.013_002';
+$VERSION = '0.013_003';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 $DEBUG = 0;
