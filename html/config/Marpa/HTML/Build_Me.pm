0f19e79b257d0f45b4d7da446fefbaf2a3f9471a
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-09-07 18:52:39 -0700

    Preparing new Marpa::HTML release

---
+++ html/config/Marpa/HTML/Build_Me.pm
@@ -0,0 +1,58 @@
+
+use 5.010;
+
+BEGIN { say STDERR __FILE__ };
+
+package Marpa::HTML::Build_Me;
+
+use strict;
+use warnings;
+
+@Marpa::HTML::Build_Me::ISA = ('Module::Build');
+
+use Config;
+use Module::Build;
+use English qw( -no_match_vars );
+
+sub ACTION_manifest {
+    die qq{Automatic generation of the MANIFEST file is disabled\n}
+	. qq{The Marpa MANIFEST file is handwritten\n};
+}
+
+sub ACTION_licensecheck {
+    my $license_pm = 'config/Marpa/HTML/License.pm';
+    require $license_pm or die "Cannot load $license_pm";
+    my @manifest = do {
+	open my $fh, q{<}, 'MANIFEST';
+	local $RS = undef;
+	my $text = <$fh>;
+	$text =~ s/[#] [^\n]* $//gxms;
+	grep { defined and not / \A \s* \z /xms } split '\n', $text;
+    };
+    my @license_problems =
+	Marpa::HTML::License::license_problems(@manifest);
+    if (@license_problems) {
+	print STDERR join q{}, @license_problems;
+	die qq{Fatal error due to license language issues};
+    }
+}
+
+sub ACTION_distcheck {
+    my $self = shift;
+    $self->ACTION_licensecheck();
+    $self->SUPER::ACTION_distcheck;
+}
+
+sub ACTION_dist {
+    my $self = shift;
+    my $changes = do {
+	open my $fh, q{<}, 'Changes';
+	local $RS = undef;
+	<$fh>;
+    };
+    my $marpa_xs_version = $self->dist_version();
+    die qq{"$marpa_xs_version" not in Changes file}
+	if 0 > index $changes, $marpa_xs_version;
+    $self->SUPER::ACTION_dist;
+} ## end sub ACTION_dist
+
