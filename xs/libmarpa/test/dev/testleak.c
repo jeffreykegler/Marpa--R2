c3d045151f8ea5965f9be3be0145449b24f64687
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-09-11 22:46:42 -0700

    Work on leak tester

--- xs/libmarpa/test/dev/testleak.c
+++ xs/libmarpa/test/dev/testleak.c
@@ -6,24 +6,33 @@
 #include <glib.h>
 #include "marpa.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
+    int i;
     Marpa_Symbol_ID S, A, a, E;
     struct marpa_g* g;
     struct marpa_r* r;
     void *result;
     /* Longest rule is 4 symbols */
     Marpa_Symbol_ID rhs[4];
+    int initial_sleep = 0;
     /* Try to move gslice area out of the
        tree of Marpa calls */
     void* dummy = g_slice_alloc(42);
+    if (argc >= 2) {
+        initial_sleep = atoi(argv[1]);
+    }
     g_slice_free1(42, dummy);
     g = marpa_g_new();
     S = marpa_symbol_new(g);
     A = marpa_symbol_new(g);
     a = marpa_symbol_new(g);
     E = marpa_symbol_new(g);
-    sleep(60);
+    for (i = initial_sleep; i > 0; i--) {
+	sleep(2);
+	fputs("-", stderr);
+    }
+    fputs("\n", stderr);
     rhs[0] = A;
     rhs[1] = A;
     rhs[2] = A;
@@ -46,12 +55,39 @@ int main(void)
         puts(marpa_r_error(r));
 	exit(1);
     }
+    if (!marpa_start_input(r)) {
+        puts(marpa_r_error(r));
+	exit(1);
+    }
+    for (i = 0; i < 4; i++) {
+	int status = marpa_alternative(r, a, GINT_TO_POINTER(42), 1);
+	if (status < 0) {
+	   printf("marpa_alternative returned %d: %s", status,
+	    marpa_r_error(r));
+	   exit(1);
+	}
+	status = marpa_earleme_complete(r);
+	if (status < 0) {
+	   printf("marpa_earleme_complete returned %d: %s", status,
+	    marpa_r_error(r));
+	   exit(1);
+	}
+    }
+    for (i = 0; i < 4; i++) {
+	int status = marpa_bocage_new(r, -1, i);
+	if (status < 0) {
+	   printf("marpa_bocage_new returned %d: %s", status,
+	    marpa_r_error(r));
+	   exit(1);
+	}
+	marpa_bocage_free(r);
+    }
     marpa_r_free(r);
     marpa_g_free(g);
     g = NULL;
     while(1) {
 	putc('.', stderr);
-	sleep(60);
+	sleep(10);
     }
 }
 
