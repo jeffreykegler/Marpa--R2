3425fee79069893ecd640007066dd565b7cff2b1
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-10-04 09:36:02 -0700

    Preparing Marpa-PP-0.009_003

--- pp/lib/Marpa/PP.pm
+++ pp/lib/Marpa/PP.pm
@@ -19,7 +19,7 @@ use 5.010;
 use strict;
 use warnings;
 use vars qw($VERSION $STRING_VERSION $DEBUG);
-$VERSION = '0.009_002';
+$VERSION = '0.009_003';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 $DEBUG = 0;
