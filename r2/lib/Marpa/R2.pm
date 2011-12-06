8a484ae4f4a1a4e355ae706adc794b2bff644ec6
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-12-05 22:10:38 -0800

    CPAN Marpa-R2-0.001_009

--- r2/lib/Marpa/R2.pm
+++ r2/lib/Marpa/R2.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '0.001_008';
+$VERSION        = '0.001_009';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- r2/lib/Marpa/R2/Value.pm
+++ r2/lib/Marpa/R2/Value.pm
@@ -21,7 +21,7 @@ use strict;
 use integer;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '0.001_008';
+$VERSION        = '0.001_009';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
