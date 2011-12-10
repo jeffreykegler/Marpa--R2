fb42bcc37497dd3601dc35caa0687c69439f5061
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-12-10 08:24:16 -0800

    Work on new libmarpa error handling: t+

--- r2/libmarpa/dev/texi2code.pl
+++ r2/libmarpa/dev/texi2code.pl
@@ -1,5 +1,4 @@
 #!perl
-#!/usr/bin/perl
 # Copyright 2011 Jeffrey Kegler
 # This file is part of Marpa::R2.  Marpa::R2 is free software: you can
 # redistribute it and/or modify it under the terms of the GNU Lesser
@@ -19,8 +18,52 @@ use 5.010;
 use strict;
 use warnings;
 use English qw( -no_match_vars );
+use Fatal qw(open close);
 
-say <<'PREAMBLE';
+if (scalar @ARGV != 3) {
+    die("usage: $PROGRAM_NAME api.h.in api.c.in error.list");
+}
+
+open my $api_h_in, '>', $ARGV[0];
+open my $api_c_in, '>', $ARGV[1];
+open my $error_list, '>', $ARGV[2];
+
+my @errors = ();
+my $error_count = 0;
+my @defs = ();
+my $pending_error_list_line = undef;
+while ( my $line = <STDIN> ) {
+    if ( $pending_error_list_line ) {
+	my ($message) = ($line =~ /Suggested \s* message [:] \s " ([^"]*) " /xms );
+	if ($message) {
+	    say {$error_list} $pending_error_list_line. $message;
+	    $pending_error_list_line = undef;
+	}
+    }
+    if ( $line =~ /[@]deftypevr/xms ) {
+        my ($error) = ($line =~ m/(MARPA_ERR_.*)\b/xms);
+	if ($error) {
+	    $pending_error_list_line = $error_count . " ";
+	    $errors[$error_count++] = $error;
+	}
+    }
+    if ( $line =~ /[@]deftypefun/xms ) {
+        my $def = q{};
+        while ( $line =~ / [@] \s* \z /xms ) {
+            $def .= $line;
+            $def =~ s/ [@] \s* \z//xms;
+            $line = <STDIN>;
+        }
+        $def .= $line;
+        $def =~ s/\A \s* [@] deftypefun \s* //xms;
+        $def =~ s/ [@]var[{] ([^}]*) [}]/$1/xmsg;
+        $def =~ s/\s+/ /xmsg;
+        $def =~ s/\s \z/;/xmsg;
+        push @defs, $def;
+    } ## end if ( $line =~ /[@]deftypefun/xms )
+} ## end while ( my $line = <STDIN> )
+
+my $preamble = <<'PREAMBLE';
 /*
  * Copyright 2011 Jeffrey Kegler
  * This file is part of Marpa::R2.  Marpa::R2 is free software: you can
@@ -39,25 +82,24 @@ say <<'PREAMBLE';
  */
 /*
  * DO NOT EDIT DIRECTLY
- * This file is written by ctangle
+ * This file is written by texi2proto.pl
  * It is not intended to be modified directly
  */
+
 PREAMBLE
 
-while ( my $line = <STDIN> ) {
-    if ( $line =~ /[@]deftypefun/xms ) {
-        my $def = q{};
-        while ( $line =~ / [@] \s* \z /xms ) {
-            $def .= $line;
-            $def =~ s/ [@] \s* \z//xms;
-            $line = <STDIN>;
-        }
-        $def .= $line;
-        $def =~ s/\A \s* [@] deftypefun \s* //xms;
-        $def =~ s/ [@]var[{] ([^}]*) [}]/$1/xmsg;
-        $def =~ s/\s+/ /xmsg;
-        $def =~ s/\s \z/;/xmsg;
-        say $def;
-    } ## end if ( $line =~ /[@]deftypefun/xms )
-} ## end while ( my $line = <STDIN> )
+say {$api_h_in} $preamble;
+say {$api_h_in} join "\n", @defs;
+say {$api_h_in} "extern const char* s_marpa_error_description[];";
+for (my $error_number = 0; $error_number < $error_count; $error_number++)
+{
+   say {$api_h_in} '#define ' . $errors[$error_number] . q{ } . $error_number;
+}
 
+say {$api_c_in} $preamble;
+say {$api_c_in} "const char* s_marpa_error_description[$error_count] = {";
+my @lines = ();
+for (my $error_number = 0; $error_number < $error_count; $error_number++) {
+   push @lines, q{  "} . $errors[$error_number] . q{"};
+}
+say {$api_c_in} +(join ",\n", @lines), "\n};\n";
