bc913edba33aff6f9b1ea6eb51f669443f138692
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-12-18 18:39:21 -0800

    Bumper version number

--- r2/lib/Marpa/R2.pm
+++ r2/lib/Marpa/R2.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '0.001_010';
+$VERSION        = '0.001_011';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
