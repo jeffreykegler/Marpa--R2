f0edc3b3dcf2c5e03147b17c8cae595895dd4a4b
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-12-27 19:15:11 -0800

    Bumped version.

--- r2/lib/Marpa/R2/Grammar.pm
+++ r2/lib/Marpa/R2/Grammar.pm
@@ -32,7 +32,7 @@ use integer;
 use utf8;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '0.001_013';
+$VERSION        = '0.001_014';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
