432024b4f8d069356b3957d0b63162e2a098969c
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-01-18 22:03:29 -0800

    Packaging: t+

--- r2/inc/Marpa/R2/Build_Me.pm
+++ r2/inc/Marpa/R2/Build_Me.pm
@@ -263,14 +263,17 @@ sub do_libmarpa {
     my $build_dir = File::Spec->catdir( $base_dir, qw(libmarpa build) );
     my $stage_dir = File::Spec->catdir( $base_dir, qw(libmarpa stage) );
     -d $build_dir or mkdir $build_dir;
-    chdir $stage_dir;
-    BUILD_FILE: for my $build_file (keys %{$manifest_hash})
-    {
-	# names in MANIFEST are always UNIX-style
-	$build_file =~ s{ \A libmarpa / build / }{}xms or next BUILD_FILE;
-	my $localized_file = File::Spec->catfile( split m{/}xms, $build_file );
-	$self->copy_if_modified($localized_file, $build_dir);
-    }
+    if ( defined $self->args('from_stage') ) {
+        chdir $stage_dir;
+        BUILD_FILE: for my $build_file ( keys %{$manifest_hash} ) {
+
+            # names in MANIFEST are always UNIX-style
+            $build_file =~ s{ \A libmarpa / build / }{}xms or next BUILD_FILE;
+            my $localized_file =
+                File::Spec->catfile( split m{/}xms, $build_file );
+            $self->copy_if_modified( $localized_file, $build_dir );
+        } ## end for my $build_file ( keys %{$manifest_hash} )
+    } ## end if ( defined $self->args('from_stage') )
     chdir $cwd;
     chdir $build_dir;
 
