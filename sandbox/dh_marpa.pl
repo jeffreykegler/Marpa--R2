b8fdb2c2a7688e653e68c8034ad62c1d4b47a8fe
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-04-11 09:23:17 -0700

    Work on DH parsers

--- sandbox/dh_marpa.pl
+++ sandbox/dh_marpa.pl
@@ -14,7 +14,7 @@ if (@ARGV) {
     die "Argument not a number" if not Scalar::Util::looks_like_number($repeat);
 }
 
-sub arg2 { return $_[3]; }
+sub arg3 { return $_[4]; }
 sub do_what_I_mean {
     shift;
     my @children = grep {defined} @_;
@@ -27,8 +27,8 @@ my $grammar = Marpa::XS::Grammar->new(
         default_action   => 'main::do_what_I_mean',
         rules            => [
             [ 'sentence', [qw(element)] ],
-            [ 'string', [qw(S_count lparen text rparent)],   'main::arg2' ],
-            [ 'array',  [qw(A_count lparen elements rparent)], 'main::arg2' ],
+            [ 'string', [qw(Schar Scount lparen text rparen)],   'main::arg3' ],
+            [ 'array',  [qw(Achar Acount lparen elements rparen)], 'main::arg3' ],
             { lhs => 'elements', rhs => [qw(element)], min => 0 },
             [ 'element', [qw(string)] ],
             [ 'element', [qw(array)] ],
@@ -37,7 +37,7 @@ my $grammar = Marpa::XS::Grammar->new(
 );
 
 $grammar->precompute();
-my $recce = Marpa::XS::Recognizer->new({ grammar => $grammar});
+my $recce = Marpa::XS::Recognizer->new({ grammar => $grammar, trace_terminals=>1});
 
 my $res;
 if ($repeat) {
@@ -46,13 +46,58 @@ if ($repeat) {
     $res = 'A2(A2(S3(Hey)S13(Hello, World!))S5(Ciao!))';
 }
 
-exit 0;
+my $string_length = 0;
 
-while (length($res)) {
-}
+INPUT: for ( ;; ) {
+    my @terminals_expected = @{$recce->terminals_expected()};
 
-exit 0;
-my $received = Dumper($res);
+say "Expecting: ", join q{ }, @terminals_expected;
+
+    my $found;
+    my $expected_terminal;
+    FIND_TOKEN:
+    for my $i (0 .. $#terminals_expected) {
+	$expected_terminal = $terminals_expected[$i];
+        if ( 'Scount' eq $expected_terminal ) {
+            $res =~ s{\A \d+}{$string_length = $found = ${^MATCH}, ''}epxmsg and last FIND_TOKEN;
+        } ## end if ( 'Scount' eq $expected_terminal )
+        if ( 'Acount' eq $expected_terminal ) {
+            $res =~ s{\A \d+}{$found = ${^MATCH}, ''}epxmsg and last FIND_TOKEN;
+        }
+        if ( 'Achar' eq $expected_terminal ) {
+            $res =~ s{\A A}{$found = ${^MATCH}, ''}epxmsg and last FIND_TOKEN;
+        }
+        if ( 'Schar' eq $expected_terminal ) {
+            $res =~ s{\A S}{$found = ${^MATCH}, ''}epxmsg and last FIND_TOKEN;
+        }
+        if ( 'lparen' eq $expected_terminal ) {
+            $res =~ s{\A [(]}{$found = ${^MATCH}, ''}epxmsg and last FIND_TOKEN;
+	}
+        if ( 'rparen' eq $expected_terminal ) {
+            $res =~ s{\A [)]}{$found = ${^MATCH}, ''}epxmsg and last FIND_TOKEN;
+	}
+        if ( 'text' eq $expected_terminal ) {
+	     $found = substr($res, 0, $string_length);
+	     $res = substr($res, $string_length);
+	     last FIND_TOKEN;
+	}
+    } ## end while ( defined( $expected_terminal = shift @{...}))
+    if (not defined $found) {
+	die("Error reading input: ",
+	    substr( $res, 0, 100 ),
+	    "\nWas expecting ",
+	    join q{ }, @terminals_expected
+	);
+    }
+say "Found ", $found;
+    $recce->read($expected_terminal, $found);
+    last INPUT if length $res <= 0;
+} ## end for ( ;; )
+
+my $result = $recce->value();
+die "No parse" if not defined $result;
+my $received = Dumper(${$result});
+
 my $expected = <<'EXPECTED_OUTPUT';
 $VAR1 = [
           [
@@ -62,7 +107,7 @@ $VAR1 = [
           'Ciao!'
         ];
 EXPECTED_OUTPUT
-if ( $received eq $expected )
+if ($received eq $expected )
 {
     say "Output matches";
 } else {
