ec0e5005c5721347a70cc5bf3e756274562464cb
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-04-12 16:25:32 -0700

    Bump version numbers

--- r2/html/lib/Marpa/R2/HTML/Callback.pm
+++ r2/html/lib/Marpa/R2/HTML/Callback.pm
@@ -21,7 +21,7 @@ use integer;
 package Marpa::R2::HTML::Callback;
 
 use vars qw( $VERSION $STRING_VERSION );
-$VERSION = '0.001_033';
+$VERSION = '0.001_034';
 $STRING_VERSION = $VERSION;
 ## use critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- r2/lib/Marpa/R2/Internal.pm
+++ r2/lib/Marpa/R2/Internal.pm
@@ -22,7 +22,7 @@ use integer;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '0.001_033';
+$VERSION        = '0.001_034';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
