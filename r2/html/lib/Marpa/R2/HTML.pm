8bc81c1710b57179cc640d01c2432408acbc8f55
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-04-21 22:02:39 -0700

    Bumped version #

--- r2/html/lib/Marpa/R2/HTML.pm
+++ r2/html/lib/Marpa/R2/HTML.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw( $VERSION $STRING_VERSION );
-$VERSION = '0.001_034';
+$VERSION = '0.001_035';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
