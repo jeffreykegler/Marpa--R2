4576fc49c9dbca7431d9af808155c1a1aecca445
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-05-26 08:47:54 -0700

    Bumped version #

--- r2/lib/Marpa/R2/Grammar.pm
+++ r2/lib/Marpa/R2/Grammar.pm
@@ -32,7 +32,7 @@ use integer;
 use utf8;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '0.001_048';
+$VERSION        = '0.002000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- r2/lib/Marpa/R2/Internal.pm
+++ r2/lib/Marpa/R2/Internal.pm
@@ -22,7 +22,7 @@ use integer;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '0.001_048';
+$VERSION        = '0.002000';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
