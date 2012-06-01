be667306481bd7322edaf18f40eb73603d6d9e09
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-06-01 08:32:44 -0700

    Bumped version #: new developer release

--- r2/html/lib/Marpa/R2/HTML.pm
+++ r2/html/lib/Marpa/R2/HTML.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw( $VERSION $STRING_VERSION );
-$VERSION        = '2.003_003';
+$VERSION        = '2.005_000';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
