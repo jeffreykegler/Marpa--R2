6ccb8d989b473a5259f63ec9f3d8d7f9776da19d
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-06-01 07:51:10 -0700

    Bump version #; public release

--- r2/lib/Marpa/R2/Value.pm
+++ r2/lib/Marpa/R2/Value.pm
@@ -21,7 +21,7 @@ use strict;
 use integer;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.003_003';
+$VERSION        = '2.004000';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
