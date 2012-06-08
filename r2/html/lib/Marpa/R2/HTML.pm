c7863ce6b3ccadc0149e44d72e9e26f7f0d51ac0
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-06-08 08:09:37 -0700

    Bump version to 2.006000

--- r2/html/lib/Marpa/R2/HTML.pm
+++ r2/html/lib/Marpa/R2/HTML.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw( $VERSION $STRING_VERSION );
-$VERSION        = '2.005_002';
+$VERSION        = '2.006000';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- r2/lib/Marpa/R2/Grammar.pm
+++ r2/lib/Marpa/R2/Grammar.pm
@@ -32,7 +32,7 @@ use integer;
 use utf8;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.005_002';
+$VERSION        = '2.006000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
