6b1b7940b90ce25e0d07fc9a638db50e83db9001
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-06-21 19:12:00 -0700

    Bumped version #

--- r2/lib/Marpa/R2/Internal.pm
+++ r2/lib/Marpa/R2/Internal.pm
@@ -22,7 +22,7 @@ use integer;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.009_001';
+$VERSION        = '2.009_002';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
