4e37f030598c01129afaba3e76d1d28a967ca4f9
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-06-22 20:37:22 -0700

    Documentation

--- methods_todo.sh
+++ methods_todo.sh
@@ -3,14 +3,15 @@ egrep '^=head2 ' r2/pod/Advanced/Thin.pod |
    perl -n -E '
      s/.*[:][:]([CGRBOTV])->new[(][)].*/marpa_\L$1\l_new/ and print;
      s/.*[\$]([cgrbotv])->([^\(]*).*/marpa_$1_$2/ and print;
-   ' > documented_method.list
+   ' > nogit/documented_method.list
 perl -n -E '
     m/::CLASS_LETTER.*['"'"']([cgrbotv])['"'"'][;]/ and $class = $1;
-    m/[\(]qw[\(]([^ \)]*)[ \)]/ and say "marpa_$class" . "_$1";
-     ' r2/xs/gp_generate.pl >> gp_method.list
+    m/[\(]qw[\(](_[^ \)]*)[ \)]/ and say $1;
+    m/[\(]qw[\(]([^_][^ \)]*)[ \)]/ and say "marpa_$class" . "_$1";
+     ' r2/xs/gp_generate.pl > nogit/gp_method.list
 perl -n -E '
   s/{[^}]*}/{}/g;
   m/^[@]deftypefn [^ ]+ [^ ]+ ([^ ]+) / and say $1;
   m/^[@]deftypefunx? [^ ]+ ([^ ]+) / and say $1;
-' r2/libmarpa/dev/api.texi | sort > libmarpa_method.list
-sort -u documented_method.list gp_method.list | comm -3 libmarpa_method.list -
+' r2/libmarpa/dev/api.texi | sort > nogit/libmarpa_method.list
+sort -u nogit/documented_method.list nogit/gp_method.list | comm -3 nogit/libmarpa_method.list -
