7fc74162885360abd489b0c506c84a1b6efe69b7
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-06-24 21:43:28 -0700

    Catalan series script for blog

---
+++ sandbox/MSR_catalan.pl
@@ -0,0 +1,50 @@
+#!perl
+use 5.010;
+use strict;
+use warnings;
+
+use Marpa::XS;
+use MarpaX::Simple::Rules 'parse_rules';
+
+sub numbers {
+    my ( undef, @numbers ) = @_;
+    return \@numbers;
+}
+
+my $catalan_rules = parse_rules(<<"RULES");
+     pair ::= a a
+     pair ::= pair a
+     pair ::= a pair
+     pair ::= pair pair
+RULES
+
+my $grammar = Marpa::XS::Grammar->new(
+    {   start   => 'pair',
+        rules   => $catalan_rules,
+    }
+);
+$grammar->precompute();
+
+sub do_catalan
+{
+    my $n           = shift;
+    my $parse_count = 0;
+    my $recce = Marpa::XS::Recognizer->new( { grammar => $grammar } );
+    # Just in case
+    $recce->set( { max_parses => 999, } );
+    for my $token_ix ( 0 .. $n - 1 ) {
+        $recce->read('a') or die "Cannot read char $token_ix";
+    }
+    $parse_count++ while $recce->value();
+    return $parse_count;
+}
+
+my @catalan_numbers = ( 0, 1, 1, 2, 5, 14, 42, 132, 429 );
+
+my $expected = join q{ }, @catalan_numbers;
+my $actual = join q{ }, 0, 1, map { do_catalan($_) } 2 .. 8 ;
+
+say "Expected: $expected";
+say "  Actual: $actual";
+say ($actual eq $expected ? 'OK' : 'MISMATCH');
+
