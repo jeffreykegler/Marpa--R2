e617c49c26f3c0fea0c93025d3b02857daa91f71
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-06-30 19:52:58 -0700

    Bumped version #: t+

--- r2/html/lib/Marpa/R2/HTML/Callback.pm
+++ r2/html/lib/Marpa/R2/HTML/Callback.pm
@@ -21,7 +21,7 @@ use integer;
 package Marpa::R2::HTML::Callback;
 
 use vars qw( $VERSION $STRING_VERSION );
-$VERSION = '2.009_004';
+$VERSION = '2.009_005';
 $STRING_VERSION = $VERSION;
 ## use critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- r2/lib/Marpa/R2/Grammar.pm
+++ r2/lib/Marpa/R2/Grammar.pm
@@ -32,7 +32,7 @@ use integer;
 use utf8;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.009_004';
+$VERSION        = '2.009_005';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
