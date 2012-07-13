00b258d8d8398f275bb831ce0fce15e46e2f8a94
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-07-13 13:49:09 -0700

    Work on new Dyck-Hollerith blog post

---
+++ blog/dyck_hollerith/dh_r2.pl
@@ -0,0 +1,95 @@
+use 5.010;
+use strict;
+use warnings;
+use Data::Dumper;
+use Scalar::Util;
+use Marpa::R2;
+
+# A Marpa::R2 parser for the Dyck-Hollerith language
+
+my $repeat;
+if (@ARGV) {
+    $repeat = $ARGV[0];
+    die "Argument not a number" if not Scalar::Util::looks_like_number($repeat);
+}
+
+sub arg1 { return $_[1]; }
+sub arg4 { return $_[4]; }
+sub all_args { shift; return \@_; }
+
+my $grammar = Marpa::R2::Grammar->new(
+    {   start            => 'sentence',
+        rules            => [
+            [ 'sentence', [qw(element)], 'main::arg1' ],
+            [ 'string', [qw(Schar Scount lparen text rparen)],   'main::arg4' ],
+            [ 'array',  [qw(Achar Acount lparen elements rparen)], 'main::arg4' ],
+            { lhs => 'elements', rhs => [qw(element)], min => 1, action=>'main::all_args' },
+            [ 'element', [qw(string)], 'main::arg1' ],
+            [ 'element', [qw(array)], 'main::arg1' ],
+        ]
+    }
+);
+
+$grammar->precompute();
+my $recce = Marpa::R2::Recognizer->new({ grammar => $grammar });
+
+my $res;
+if ($repeat) {
+    $res = "A$repeat(" . ('A2(A2(S3(Hey)S13(Hello, World!))S5(Ciao!))' x $repeat) . ')';
+} else {
+    $res = 'A2(A2(S3(Hey)S13(Hello, World!))S5(Ciao!))';
+}
+
+my $string_length = 0;
+my $position = 0;
+my $input_length = length $res;
+
+INPUT: while ($position < $input_length) {
+	pos $res = $position;
+	if ($res =~ m/\G S (\d+) [(]/xms) {
+            my $string_length = $1;
+	    $recce->read( 'Schar');
+	    $recce->read( 'Scount' );
+	    $recce->read( 'lparen' );
+	    $position += 2 + (length $string_length);
+	    $recce->read( 'text', substr( $res, $position, $string_length ));
+	    $position += $string_length;
+            next INPUT;
+        }
+	if ($res =~ m/\G A (\d+) [(]/xms) {
+            my $count = $1;
+	    $recce->read( 'Achar');
+	    $recce->read( 'Acount' );
+	    $recce->read( 'lparen' );
+	    $position += 2 + length $count;
+            next INPUT;
+        }
+        if ( $res =~ m{\G [)] }xms ) {
+	    $recce->read( 'rparen' );
+	    $position += 1;
+            next INPUT;
+        }
+        die "Error reading input: ", substr( $res, $position, 100 );
+} ## end for ( ;; )
+
+my $result = $recce->value();
+die "No parse" if not defined $result;
+my $received = Dumper(${$result});
+
+my $expected = <<'EXPECTED_OUTPUT';
+$VAR1 = [
+          [
+            'Hey',
+            'Hello, World!'
+          ],
+          'Ciao!'
+        ];
+EXPECTED_OUTPUT
+if ($received eq $expected )
+{
+    say "Output matches";
+} else {
+    say "Output differs: $received";
+}
+
+
---
+++ blog/dyck_hollerith/dh_thin.pl
@@ -0,0 +1,137 @@
+use 5.010;
+use strict;
+use warnings;
+use Data::Dumper;
+use Scalar::Util;
+use Marpa::R2;
+
+# A Marpa::R2 parser for the Dyck-Hollerith language
+
+my $repeat;
+if (@ARGV) {
+    $repeat = $ARGV[0];
+    die "Argument not a number" if not Scalar::Util::looks_like_number($repeat);
+}
+
+sub arg1 { return $_[1]; }
+sub arg4 { return $_[4]; }
+sub all_args { shift; return \@_; }
+
+my $grammar = Marpa::R2::Thin::G->new( { if =>1 } );
+my $sentence_symbol = $grammar->symbol_new();
+my $string_symbol = $grammar->symbol_new();
+my $array_symbol = $grammar->symbol_new();
+my $elements_symbol = $grammar->symbol_new();
+my $element_symbol = $grammar->symbol_new();
+my $Schar_symbol = $grammar->symbol_new();
+my $Scount_symbol = $grammar->symbol_new();
+my $Achar_symbol = $grammar->symbol_new();
+my $Acount_symbol = $grammar->symbol_new();
+my $lparen_symbol = $grammar->symbol_new();
+my $rparen_symbol = $grammar->symbol_new();
+my $text_symbol = $grammar->symbol_new();
+$grammar->start_symbol_set($sentence_symbol);
+my $sentence_rule = $grammar->rule_new($sentence_symbol, [$element_symbol]);
+my $string_rule = $grammar->rule_new(
+    $string_symbol,
+    [   $Schar_symbol, $Scount_symbol, $lparen_symbol,
+        $text_symbol,  $rparen_symbol
+    ]
+);
+my $array_rule = $grammar->rule_new(
+    $array_symbol,
+    [   $Achar_symbol,    $Acount_symbol, $lparen_symbol,
+        $elements_symbol, $rparen_symbol
+    ]
+);
+my $element_string_rule =
+    $grammar->rule_new( $element_symbol, [$string_symbol] );
+my $element_array_rule =
+    $grammar->rule_new( $element_symbol, [$array_symbol] );
+my $elements_rule =
+    $grammar->sequence_new( $elements_symbol, $element_symbol, { min => 1 } );
+$grammar->precompute();
+
+my $recce = Marpa::R2::Thin::R->new( $grammar);
+$recce->start_input();
+
+my $res;
+if ($repeat) {
+    $res = "A$repeat(" . ('A2(A2(S3(Hey)S13(Hello, World!))S5(Ciao!))' x $repeat) . ')';
+} else {
+    $res = 'A2(A2(S3(Hey)S13(Hello, World!))S5(Ciao!))';
+}
+
+my $string_length = 0;
+my $position = 0;
+my $input_length = length $res;
+
+my @token_values = ('Do not use position zero');
+
+INPUT: while ($position < $input_length) {
+	pos $res = $position;
+	if ($res =~ m/\G S (\d+) [(]/xms) {
+            my $string_length = $1;
+	    $recce->alternative( $Schar_symbol, 0, 1); $recce->earleme_complete();
+	    $recce->alternative( $Scount_symbol, 0, 1 ); $recce->earleme_complete();
+	    $recce->alternative( $lparen_symbol, 0, 1 ); $recce->earleme_complete();
+	    $position += 2 + (length $string_length);
+	    my $token_ix = 1 + push @token_values, substr( $res, $position, $string_length );
+	    $recce->alternative( $text_symbol, $token_ix, 1);
+	    $recce->earleme_complete();
+	    $position += $string_length;
+            next INPUT;
+        }
+	if ($res =~ m/\G A (\d+) [(]/xms) {
+            my $count = $1;
+	    $recce->alternative( $Achar_symbol, 0, 1); $recce->earleme_complete();
+	    $recce->alternative( $Acount_symbol, 0, 1 ); $recce->earleme_complete();
+	    $recce->alternative( $lparen_symbol, 0, 1 ); $recce->earleme_complete();
+	    $position += 2 + length $count;
+            next INPUT;
+        }
+        if ( $res =~ m{\G [)] }xms ) {
+	    $recce->alternative( $rparen_symbol, 0, 1 ); $recce->earleme_complete();
+	    $position += 1;
+            next INPUT;
+        }
+        die "Error reading input: ", substr( $res, $position, 100 );
+} ## end for ( ;; )
+
+my $latest_earley_set_ID = $recce->latest_earley_set();
+my $bocage        = Marpa::R2::Thin::B->new( $recce, $latest_earley_set_ID );
+my $order         = Marpa::R2::Thin::O->new($bocage);
+my $tree          = Marpa::R2::Thin::T->new($order);
+if (not defined $tree->next()) { die "No parse" }
+my $valuator          = Marpa::R2::Thin::V->new($tree);
+$valuator->rule_is_valued_set($sentence_rule, 1);
+$valuator->rule_is_valued_set($string_rule, 1);
+$valuator->rule_is_valued_set($array_rule, 1);
+$valuator->rule_is_valued_set($element_string_rule, 1);
+$valuator->rule_is_valued_set($element_array_rule, 1);
+$valuator->rule_is_valued_set($elements_rule, 1);
+while (1) {
+   my @data = $valuator->step();
+   last if not defined $data[0];
+   say join " ", @data;
+}
+
+exit 0;
+
+# my $received = Dumper(${$result});
+# 
+# my $expected = <<'EXPECTED_OUTPUT';
+# $VAR1 = [
+#           [
+#             'Hey',
+#             'Hello, World!'
+#           ],
+#           'Ciao!'
+#         ];
+# EXPECTED_OUTPUT
+# if ($received eq $expected )
+# {
+#     say "Output matches";
+# } else {
+#     say "Output differs: $received";
+# }
