cb16003c6dc182c9338c869179f7fef5f472ef38
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-07-30 09:12:40 -0700

    Work on expected-symbol events

--- sandbox/balanced.pl
+++ sandbox/balanced.pl
@@ -82,6 +82,7 @@ sub arg1 {
 }
 
 my $marpa_answer_shown;
+my $marpa_e_answer_shown;
 my $regex_answer_shown;
 my $regex_new_answer_shown;
 
@@ -213,8 +214,98 @@ sub do_marpa_r2 {
 
 } ## end sub do_marpa_r2
 
-sub do_marpa_thin {
-}
+sub do_marpa_r2e {
+    my ($s) = @_;
+
+    my $grammar_args = {
+        start => 'S',
+        rules => [
+            [ S => [qw(prefix first_balanced endmark )] ],
+            {   lhs => 'S',
+                rhs => [qw(prefix first_balanced )]
+            },
+            { lhs => 'prefix',      rhs => [qw(prefix_char)], min => 0 },
+            { lhs => 'prefix_char', rhs => [qw(xlparen)] },
+            { lhs => 'prefix_char', rhs => [qw(rparen)] },
+            { lhs => 'lparen',      rhs => [qw(xlparen)] },
+            { lhs => 'lparen',      rhs => [qw(ilparen)] },
+            {   lhs => 'first_balanced',
+                rhs => [qw(xlparen balanced_sequence rparen)],
+            },
+            {   lhs => 'balanced',
+                rhs => [qw(lparen balanced_sequence rparen)],
+            },
+            {   lhs => 'balanced_sequence',
+                rhs => [qw(balanced)],
+                min => 0,
+            },
+        ],
+    };
+
+    my $grammar = Marpa::R2::Grammar->new($grammar_args);
+    $grammar->set( { terminals => [qw(xlparen ilparen rparen endmark )] } );
+
+    $grammar->precompute();
+
+    my ($first_balanced_rule) =
+        grep { ( $grammar->rule($_) )[0] eq 'first_balanced' }
+        $grammar->rule_ids();
+
+    my $recce         = Marpa::R2::Recognizer->new( { grammar => $grammar } );
+    my $location      = 0;
+    my $string_length = length $s;
+    my $end_of_match;
+
+    # find the match which ends first -- the one which starts
+    # first must start at or before it does
+    CHAR: while ( $location < $string_length ) {
+        my $value = substr $s, $location, 1;
+        if ( $value eq '(' ) {
+
+            # say "Adding xlparen at $location";
+            $recce->read( 'xlparen', $location );
+        }
+        else {
+            # say "Adding rparen at $location";
+            $recce->read('rparen');
+        }
+        if ( 'endmark' ~~ $recce->terminals_expected() ) {
+            $end_of_match = $location + 1;
+
+            # say "Setting end of match to ", $location + 1;
+            last CHAR;
+        } ## end if ( 'endmark' ~~ $recce->terminals_expected() )
+        $location++;
+    } ## end CHAR: while ( $location < $string_length )
+
+    if ( not defined $end_of_match ) {
+        say "No balanced parens";
+        return 0;
+    }
+
+    CHAR: while ( ++$location < $string_length ) {
+        my $value = substr $s, $location, 1;
+        my $token = $value eq '(' ? 'ilparen' : 'rparen';
+
+        # say "Adding $token at $location";
+        last CHAR if not defined $recce->read($token);
+        if ( 'endmark' ~~ $recce->terminals_expected() ) {
+            $end_of_match = $location + 1;
+        }
+    } ## end CHAR: while ( ++$location < $string_length )
+
+    my $report = $recce->progress($end_of_match);
+
+    # say Dumper($report);
+    my $start_of_match = List::Util::min map { $_->[2] }
+        grep { $_->[1] < 0 && $_->[0] eq $first_balanced_rule } @{$report};
+    my $value = substr $s, $start_of_match, $end_of_match - $start_of_match;
+    return 0 if $marpa_e_answer_shown;
+    $marpa_e_answer_shown = $value;
+    say qq{marpa r2e: "$value" at $start_of_match-$end_of_match};
+    return 0;
+
+} ## end sub do_marpa_r2e
 
 sub do_regex {
     my ($s) = @_;
@@ -244,8 +335,10 @@ sub do_regex_new {
 # say timestr countit( 2, sub { do_regex($s) } );
 # say timestr countit( 2, sub { do_regex_new($s) } );
 Benchmark::cmpthese ( -4, {
+    marpa_r2e => sub { do_marpa_r2e($s) },
     marpa_r2 => sub { do_marpa_r2($s) },
     regex => sub { do_regex_new($s) }
 } );
 
 say +($marpa_answer_shown eq $regex_new_answer_shown ? 'New Answer matches' : 'NEW ANSWER DOES NOT MATCH!');
+say +($marpa_e_answer_shown eq $regex_new_answer_shown ? 'New Answer matches' : 'NEW ANSWER DOES NOT MATCH!');
