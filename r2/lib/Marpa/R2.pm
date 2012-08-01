d145121b1b10b4f7968b2a09775944d8ba25959e
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-08-01 09:21:53 -0700

    Bumped version to 2.015_003

--- r2/lib/Marpa/R2.pm
+++ r2/lib/Marpa/R2.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '2.015_002';
+$VERSION        = '2.015_003';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- r2/lib/Marpa/R2/Value.pm
+++ r2/lib/Marpa/R2/Value.pm
@@ -21,7 +21,7 @@ use strict;
 use integer;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.015_002';
+$VERSION        = '2.015_003';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
