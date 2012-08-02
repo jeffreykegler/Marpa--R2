7e5b8955ce2bf774bf4a4f30781f1ddf0e786431
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-08-02 16:36:05 -0700

    Bumper version # to 2.015_004

--- r2/html/lib/Marpa/R2/HTML/Callback.pm
+++ r2/html/lib/Marpa/R2/HTML/Callback.pm
@@ -21,7 +21,7 @@ use integer;
 package Marpa::R2::HTML::Callback;
 
 use vars qw( $VERSION $STRING_VERSION );
-$VERSION = '2.015_003';
+$VERSION = '2.015_004';
 $STRING_VERSION = $VERSION;
 ## use critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- r2/lib/Marpa/R2/Value.pm
+++ r2/lib/Marpa/R2/Value.pm
@@ -21,7 +21,7 @@ use strict;
 use integer;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.015_003';
+$VERSION        = '2.015_004';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
