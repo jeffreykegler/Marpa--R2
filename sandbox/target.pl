b543a5ede054851154934a6a97b6dadb4989a73d
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-08-04 21:10:20 -0700

    Work on target.pl

--- sandbox/target.pl
+++ sandbox/target.pl
@@ -12,7 +12,7 @@ my $string;
 my $pp              = 0;
 my $do_regex        = 0;
 my $do_thin         = 1;
-my $do_target         = 0;
+my $do_retrace         = 1;
 my $do_r2           = 1;
 my $do_timing = 1;
 my $random = 0;
@@ -25,7 +25,7 @@ my $getopt_result   = GetOptions(
     "random!" => \$random,
     "regex!"    => \$do_regex,
     "thin!"     => \$do_thin,
-    "target!"     => \$do_target,
+    "retrace!"     => \$do_retrace,
     "r2!"       => \$do_r2,
     "time!"       => \$do_timing,
 );
@@ -104,7 +104,7 @@ sub arg1 {
 my $marpa_answer_shown;
 my $flm_answer_shown;
 my $thin_answer_shown;
-my $target_answer_shown;
+my $retrace_answer_shown;
 my $regex_old_answer_shown;
 my $regex_answer_shown;
 
@@ -232,7 +232,7 @@ sub do_r2 {
     my $value = substr $s, $start_of_match, $end_of_match - $start_of_match;
     return 0 if $marpa_answer_shown;
     $marpa_answer_shown = $value;
-    say qq{marpa: "$value" at $start_of_match-$end_of_match};
+    say qq{r2: "$value" at $start_of_match-$end_of_match};
     return 0;
 
 } ## end sub do_r2e
@@ -368,37 +368,39 @@ sub do_thin {
 
 } ## end sub do_thin
 
-sub do_target {
+sub do_retrace {
     my ($s) = @_;
 
     my $grammar = Marpa::R2::Grammar->new($grammar_args);
 
-    my $target_grammar        = Marpa::R2::Thin::G->new( { if => 1 } );
-    my $s_start = $target_grammar->symbol_new();
-    my $s_target_end_marker      = $target_grammar->symbol_new();
-    my $s_target      = $target_grammar->symbol_new();
-    my $s_prefix            = $target_grammar->symbol_new();
-    my $s_prefix_char       = $target_grammar->symbol_new();
-    $target_grammar->start_symbol_set($s_start);
-    $target_grammar->rule_new( $s_start,
+    my $retrace_grammar     = Marpa::R2::Thin::G->new( { if => 1 } );
+    my $s_start             = $retrace_grammar->symbol_new();
+    my $s_target_end_marker = $retrace_grammar->symbol_new();
+    my $s_target            = $retrace_grammar->symbol_new();
+    my $s_prefix            = $retrace_grammar->symbol_new();
+    my $s_prefix_char       = $retrace_grammar->symbol_new();
+    $retrace_grammar->start_symbol_set($s_start);
+    $retrace_grammar->rule_new( $s_start,
         [ $s_prefix, $s_target, $s_target_end_marker ] );
-    $target_grammar->sequence_new( $s_prefix, $s_prefix_char, { min => 0 } );
+    $retrace_grammar->sequence_new( $s_prefix, $s_prefix_char, { min => 0 } );
 
-    my $s_lparen                  = $target_grammar->symbol_new();
-    my $s_rparen                  = $target_grammar->symbol_new();
-    my $s_balanced_paren_sequence = $target_grammar->symbol_new();
-    my $s_balanced_parens         = $target_grammar->symbol_new();
-    my $target_rule = $target_grammar->rule_new( $s_target, [ $s_balanced_parens ] );
-    $target_grammar->sequence_new( $s_balanced_paren_sequence, $s_balanced_parens,
-        { min => 0 } );
-    $target_grammar->rule_new( $s_balanced_parens,
+    my $s_lparen                  = $retrace_grammar->symbol_new();
+    my $s_rparen                  = $retrace_grammar->symbol_new();
+    my $s_balanced_paren_sequence = $retrace_grammar->symbol_new();
+    my $s_balanced_parens         = $retrace_grammar->symbol_new();
+    my $target_rule =
+        $retrace_grammar->rule_new( $s_target, [$s_balanced_parens] );
+    $retrace_grammar->sequence_new( $s_balanced_paren_sequence,
+        $s_balanced_parens, { min => 0 } );
+    $retrace_grammar->rule_new( $s_balanced_parens,
         [ $s_lparen, $s_balanced_paren_sequence, $s_rparen ] );
 
-    $target_grammar->precompute();
+    $retrace_grammar->precompute();
 
-    my $target_recce = Marpa::R2::Thin::R->new($target_grammar);
-    $target_recce->start_input();
-    $target_recce->expected_symbol_event_set( $s_target_end_marker, 1 );
+    my $retrace_recce = Marpa::R2::Thin::R->new($retrace_grammar);
+    $retrace_recce->start_input();
+    $retrace_recce->expected_symbol_event_set( $s_target_end_marker, 1 );
+    $retrace_recce->ruby_slippers_set(1);
 
     my $location      = 0;
     my $string_length = length $s;
@@ -411,13 +413,13 @@ sub do_target {
     CHAR: while ( $location < $string_length ) {
         my $value = substr $s, $location, 1;
         my $event_count;
-	$target_recce->alternative( $s_prefix_char, 0, 1);
-        $value eq '(' and $target_recce->alternative( $s_lparen, 0, 1 );
-        $value eq ')' and $target_recce->alternative( $s_rparen, 0, 1 );
-	$event_count = $target_recce->earleme_complete();
+        $retrace_recce->alternative( $s_prefix_char, 0, 1 );
+        $value eq '(' and $retrace_recce->alternative( $s_lparen, 0, 1 );
+        $value eq ')' and $retrace_recce->alternative( $s_rparen, 0, 1 );
+        $event_count = $retrace_recce->earleme_complete();
         if ($event_count
             and grep { $_ eq 'MARPA_EVENT_SYMBOL_EXPECTED' }
-            map { ; ( $target_grammar->event($_) )[0] }
+            map { ; ( $retrace_grammar->event($_) )[0] }
             ( 0 .. $event_count - 1 )
             )
         {
@@ -433,79 +435,86 @@ sub do_target {
     }
     my $start_of_match_earleme = $end_of_match_earleme - 1;
 
-    $target_recce->progress_report_start($end_of_match_earleme);
+    $retrace_recce->progress_report_start($end_of_match_earleme);
     ITEM: while (1) {
         my ( $rule_id, $dot_position, $item_origin ) =
-            $target_recce->progress_item();
+            $retrace_recce->progress_item();
         last ITEM if not defined $rule_id;
         next ITEM if $dot_position >= 0;
         next ITEM if $rule_id != $target_rule;
-        $start_of_match_earleme = $item_origin if $item_origin < $start_of_match_earleme;
+        $start_of_match_earleme = $item_origin
+            if $item_origin < $start_of_match_earleme;
     } ## end ITEM: while (1)
 
     # Start the recognizer over again
-    $target_recce = Marpa::R2::Thin::R->new($target_grammar);
-    $target_recce->start_input();
+    $retrace_recce = Marpa::R2::Thin::R->new($retrace_grammar);
+    $retrace_recce->start_input();
+    $retrace_recce->ruby_slippers_set(1);
 
     # Redo the prefix -- we know this must succeed, so no checking
     $location = 0;
     CHAR: while ( $location < $start_of_match_earleme ) {
         my $value = substr $s, $location, 1;
-	$target_recce->alternative( $s_prefix_char, 0, 1);
-        $value eq '(' and $target_recce->alternative( $s_lparen, 0, 1 );
-        $value eq ')' and $target_recce->alternative( $s_rparen, 0, 1 );
-	$target_recce->earleme_complete();
+        $retrace_recce->alternative( $s_prefix_char, 0, 1 );
+        $value eq '(' and $retrace_recce->alternative( $s_lparen, 0, 1 );
+        $value eq ')' and $retrace_recce->alternative( $s_rparen, 0, 1 );
+        $retrace_recce->earleme_complete();
         $location++;
-    } ## end CHAR: while ( $location < $string_length )
+    } ## end CHAR: while ( $location < $start_of_match_earleme )
 
-    $target_recce->expected_symbol_event_set( $s_target_end_marker, 1 );
-    $target_recce->ruby_slippers_set(1);
+    $retrace_recce->expected_symbol_event_set( $s_target_end_marker, 1 );
 
     # We are after the prefix, so now we just continue until exhausted
     CHAR: while ( $location < $string_length ) {
         my $value = substr $s, $location, 1;
         last CHAR
-            if $target_recce->alternative(
-                    ( $value eq '(' ? $s_lparen : $s_rparen ),
-                    0, 1 );
-        my $event_count = $target_recce->earleme_complete();
+            if $retrace_recce->alternative(
+            ( $value eq '(' ? $s_lparen : $s_rparen ),
+            0, 1 );
+        my $event_count = $retrace_recce->earleme_complete();
         if ($event_count) {
-	   my $exhausted = 0;
-	   EVENT: for my $event_type (map { ($target_grammar->event($_))[0] } 0 .. $event_count-1 ) {
-	      if ( $event_type eq 'MARPA_EVENT_SYMBOL_EXPECTED' ) {
-		$end_of_match_earleme = $location + 1;
-		next EVENT;
-	      }
-	      if ( $event_type eq 'MARPA_EVENT_EXHAUSTED' ) {
-	         $exhausted = 1;
-		next EVENT;
-	      }
-	      die "Unknown event: $event_type";
-	   }
-	   last CHAR if $exhausted;
-	}
+            my $exhausted = 0;
+            EVENT:
+            for my $event_type ( map { ( $retrace_grammar->event($_) )[0] }
+                0 .. $event_count - 1 )
+            {
+                if ( $event_type eq 'MARPA_EVENT_SYMBOL_EXPECTED' ) {
+                    $end_of_match_earleme = $location + 1;
+                    next EVENT;
+                }
+                if ( $event_type eq 'MARPA_EVENT_EXHAUSTED' ) {
+                    $exhausted = 1;
+                    next EVENT;
+                }
+                die "Unknown event: $event_type";
+            } ## end for my $event_type ( map { ( $retrace_grammar->event...)})
+            last CHAR if $exhausted;
+        } ## end if ($event_count)
         $location++;
     } ## end CHAR: while ( $location < $string_length )
 
     $start_of_match_earleme = $end_of_match_earleme;
-    $target_recce->progress_report_start($end_of_match_earleme);
+    $retrace_recce->progress_report_start($end_of_match_earleme);
     ITEM: while (1) {
         my ( $rule_id, $dot_position, $item_origin ) =
-            $target_recce->progress_item();
+            $retrace_recce->progress_item();
         last ITEM if not defined $rule_id;
         next ITEM if $dot_position >= 0;
         next ITEM if $rule_id != $target_rule;
-        $start_of_match_earleme = $item_origin if $item_origin < $start_of_match_earleme;
+        $start_of_match_earleme = $item_origin
+            if $item_origin < $start_of_match_earleme;
     } ## end ITEM: while (1)
 
     my $start_of_match = $start_of_match_earleme;
-    my $value = substr $s, $start_of_match, $end_of_match_earleme - $start_of_match_earleme;
-    return 0 if $target_answer_shown;
-    $target_answer_shown = $value;
-    say qq{target: "$value" at $start_of_match_earleme-$end_of_match_earleme};
+    my $value = substr $s, $start_of_match,
+        $end_of_match_earleme - $start_of_match_earleme;
+    return 0 if $retrace_answer_shown;
+    $retrace_answer_shown = $value;
+    say
+        qq{retrace: "$value" at $start_of_match_earleme-$end_of_match_earleme};
     return 0;
 
-} ## end sub do_target
+} ## end sub do_retrace
 
 sub do_flm {
     my ($s) = @_;
@@ -645,7 +654,7 @@ my $tests = {
     flm => sub { do_flm($s) },
 };
 
-$tests->{target} = sub { do_target($s) } if $do_target;
+$tests->{retrace} = sub { do_retrace($s) } if $do_retrace;
 $tests->{regex} = sub { do_regex($s) } if $do_regex;
 $tests->{thin} = sub { do_thin($s) } if $do_thin;
 $tests->{r2} = sub { do_r2($s) } if $do_r2;
@@ -662,10 +671,10 @@ if ( !$do_timing ) {
 Benchmark::cmpthese ( $iteration_count, $tests );
 
 my $answer = '(()())';
-if ($do_target) {
-    say +( $target_answer_shown eq $answer
-        ? 'Target Answer matches'
-        : 'Target ANSWER DOES NOT MATCH!' );
+if ($do_retrace) {
+    say +( $retrace_answer_shown eq $answer
+        ? 'Retrace Answer matches'
+        : 'Retrace ANSWER DOES NOT MATCH!' );
 }
 if ($do_r2) {
   say +($marpa_answer_shown eq $answer ? 'R2 Answer matches' : 'R2 ANSWER DOES NOT MATCH!');
