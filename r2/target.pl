81f0b96c1b088b2d2c0bb2708d675fc1e8644a1f
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-08-11 09:53:52 -0700

    Work on new scanner

--- r2/target.pl
+++ r2/target.pl
@@ -15,6 +15,7 @@ my $do_regex;
 my $do_thin;
 my $do_thinsl;
 my $do_retrace;
+my $do_resl;
 my $do_r2;
 my $do_flm;
 my $do_timing       = 1;
@@ -30,6 +31,7 @@ my $getopt_result   = GetOptions(
     "thin!"     => \$do_thin,
     "thinsl!"   => \$do_thinsl,
     "retrace!"  => \$do_retrace,
+    "resl!"  => \$do_resl,
     "only!"     => \$do_only,
     "r2!"       => \$do_r2,
     "flm!"      => \$do_flm,
@@ -49,6 +51,7 @@ if ( !$do_only ) {
     $do_thin    //= 1;
     $do_thinsl  //= 1;
     $do_retrace //= 1;
+    $do_resl //= 1;
     $do_r2      //= 1;
     $do_flm     //= 0;
 } ## end if ( !$do_only )
@@ -131,6 +134,7 @@ my $r2_answer_shown;
 my $thin_answer_shown;
 my $thinsl_answer_shown;
 my $retrace_answer_shown;
+my $resl_answer_shown;
 my $regex_old_answer_shown;
 my $regex_answer_shown;
 
@@ -509,6 +513,159 @@ sub do_thinsl {
 
 } ## end sub do_thinsl
 
+sub do_resl {
+    my ($s) = @_;
+
+    my $resl_grammar     = Marpa::R2::Thin::G->new( { if => 1 } );
+    my $s_start             = $resl_grammar->symbol_new();
+    my $s_target_end_marker = $resl_grammar->symbol_new();
+    my $s_target            = $resl_grammar->symbol_new();
+    my $s_prefix            = $resl_grammar->symbol_new();
+    my $s_prefix_char       = $resl_grammar->symbol_new();
+    $resl_grammar->start_symbol_set($s_start);
+    $resl_grammar->rule_new( $s_start,
+        [ $s_prefix, $s_target, $s_target_end_marker ] );
+    $resl_grammar->sequence_new( $s_prefix, $s_prefix_char, { min => 0 } );
+
+    my $s_lparen                  = $resl_grammar->symbol_new();
+    my $s_rparen                  = $resl_grammar->symbol_new();
+    my $s_balanced_paren_sequence = $resl_grammar->symbol_new();
+    my $s_balanced_parens         = $resl_grammar->symbol_new();
+    my $target_rule =
+        $resl_grammar->rule_new( $s_target, [$s_balanced_parens] );
+    $resl_grammar->sequence_new( $s_balanced_paren_sequence,
+        $s_balanced_parens, { min => 0 } );
+    $resl_grammar->rule_new( $s_balanced_parens,
+        [ $s_lparen, $s_balanced_paren_sequence, $s_rparen ] );
+
+    $resl_grammar->precompute();
+
+    my $resl_recce = Marpa::R2::Thin::R->new($resl_grammar);
+    $resl_recce->start_input();
+    $resl_recce->expected_symbol_event_set( $s_target_end_marker, 1 );
+
+    $resl_recce->char_register( ord('('),
+	$op_alternative_ignore, $s_lparen,
+	$op_alternative_ignore, $s_prefix_char,
+        $op_earleme_complete );
+    $resl_recce->char_register( ord(')'), $op_alternative, $s_rparen,
+	$op_alternative_ignore, $s_prefix_char,
+        $op_earleme_complete );
+
+    my $location      = 0;
+    my $string_length = length $s;
+    my $end_of_match_earleme;
+
+    $resl_recce->input_string_set($s);
+    my $event_count = $resl_recce->input_string_read();
+    if ( not $event_count ) {
+        say "No balanced parens";
+        return 0;
+    }
+    if ( $event_count < 0 ) {
+        die "Token rejected";
+    }
+
+    if (grep { $_ eq 'MARPA_EVENT_SYMBOL_EXPECTED' }
+        map { ; ( $resl_grammar->event($_) )[0] } ( 0 .. $event_count - 1 )
+        )
+    {
+
+        die if $resl_recce->input_string_hop(1) != 1;
+        $end_of_match_earleme = $resl_recce->input_string_pos();
+    } ## end if ( grep { $_ eq 'MARPA_EVENT_SYMBOL_EXPECTED' } map...)
+
+    # Add a check that we don't already expect the end_marker
+    # at location 0? This will detect zero-length targets?
+
+    if ( not defined $end_of_match_earleme ) {
+        say "No balanced parens";
+        return 0;
+    }
+
+    my $start_of_match_earleme = $end_of_match_earleme - 1;
+
+    $resl_recce->progress_report_start($end_of_match_earleme);
+    ITEM: while (1) {
+        my ( $rule_id, $dot_position, $item_origin ) =
+            $resl_recce->progress_item();
+        last ITEM if not defined $rule_id;
+        next ITEM if $dot_position >= 0;
+        next ITEM if $rule_id != $target_rule;
+        $start_of_match_earleme = $item_origin
+            if $item_origin < $start_of_match_earleme;
+    } ## end ITEM: while (1)
+
+    # Start the recognizer over again
+    $resl_recce = Marpa::R2::Thin::R->new($resl_grammar);
+    $resl_recce->start_input();
+    $resl_recce->ruby_slippers_set(1);
+
+    # Redo the prefix -- we know this must succeed, so no checking
+    $location = 0;
+    CHAR: while ( $location < $start_of_match_earleme ) {
+        my $value = substr $s, $location, 1;
+        $resl_recce->alternative( $s_prefix_char, 0, 1 );
+        $value eq '(' and $resl_recce->alternative( $s_lparen, 0, 1 );
+        $value eq ')' and $resl_recce->alternative( $s_rparen, 0, 1 );
+        $resl_recce->earleme_complete();
+        $location++;
+    } ## end CHAR: while ( $location < $start_of_match_earleme )
+
+    $resl_recce->expected_symbol_event_set( $s_target_end_marker, 1 );
+
+    # We are after the prefix, so now we just continue until exhausted
+    CHAR: while ( $location < $string_length ) {
+        my $value = substr $s, $location, 1;
+        last CHAR
+            if $resl_recce->alternative(
+            ( $value eq '(' ? $s_lparen : $s_rparen ),
+            0, 1 );
+        my $event_count = $resl_recce->earleme_complete();
+        if ($event_count) {
+            my $exhausted = 0;
+            EVENT:
+            for my $event_type ( map { ( $resl_grammar->event($_) )[0] }
+                0 .. $event_count - 1 )
+            {
+                if ( $event_type eq 'MARPA_EVENT_SYMBOL_EXPECTED' ) {
+                    $end_of_match_earleme = $location + 1;
+                    next EVENT;
+                }
+                if ( $event_type eq 'MARPA_EVENT_EXHAUSTED' ) {
+                    $exhausted = 1;
+                    next EVENT;
+                }
+                die "Unknown event: $event_type";
+            } ## end for my $event_type ( map { ( $resl_grammar->event...)})
+            last CHAR if $exhausted;
+        } ## end if ($event_count)
+        $location++;
+    } ## end CHAR: while ( $location < $string_length )
+
+    $start_of_match_earleme = $end_of_match_earleme;
+    $resl_recce->progress_report_start($end_of_match_earleme);
+    ITEM: while (1) {
+        my ( $rule_id, $dot_position, $item_origin ) =
+            $resl_recce->progress_item();
+        last ITEM if not defined $rule_id;
+        next ITEM if $dot_position >= 0;
+        next ITEM if $rule_id != $target_rule;
+        $start_of_match_earleme = $item_origin
+            if $item_origin < $start_of_match_earleme;
+    } ## end ITEM: while (1)
+
+    my $start_of_match = $start_of_match_earleme;
+    my $value = substr $s, $start_of_match,
+        $end_of_match_earleme - $start_of_match_earleme;
+    return 0 if $resl_answer_shown;
+    $resl_answer_shown = $value;
+    say
+        qq{resl: "$value" at $start_of_match_earleme-$end_of_match_earleme};
+    return 0;
+
+} ## end sub do_resl
+
 sub do_retrace {
     my ($s) = @_;
 
@@ -797,6 +954,8 @@ $tests->{flm} = sub { do_flm($s) }
     if $do_flm;
 $tests->{retrace} = sub { do_retrace($s) }
     if $do_retrace;
+$tests->{resl} = sub { do_resl($s) }
+    if $do_resl;
 $tests->{regex} = sub { do_regex($s) }
     if $do_regex;
 $tests->{thin} = sub { do_thin($s) }
@@ -826,6 +985,13 @@ if ($do_retrace) {
         : 'Retrace ANSWER DOES NOT MATCH!'
     );
 } ## end if ($do_retrace)
+if ($do_resl) {
+    say +(
+        $resl_answer_shown eq $answer
+        ? 'Resl Answer matches'
+        : 'Resl ANSWER DOES NOT MATCH!'
+    );
+}
 if ($do_flm) {
     say +( $flm_answer_shown eq $answer
         ? 'FLM Answer matches'
