9f6e0b72311a1cef792dfdb0ea824d433e134ba7
- <->
2012-09-09 19:07:54 -0700

    Bumped version to 2.019_003

--- r2/html/lib/Marpa/R2/HTML.pm
+++ r2/html/lib/Marpa/R2/HTML.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw( $VERSION $STRING_VERSION );
-$VERSION        = '2.019_002';
+$VERSION        = '2.019_003';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
