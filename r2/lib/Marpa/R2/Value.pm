b32e8fecad796bb3b7c195783ee24dd44cbf30f7
- <->
2012-09-13 08:21:52 -0700

    Bumped version # to 2.021_000

--- r2/lib/Marpa/R2/Value.pm
+++ r2/lib/Marpa/R2/Value.pm
@@ -21,7 +21,7 @@ use strict;
 use integer;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.020000';
+$VERSION        = '2.021_000';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
