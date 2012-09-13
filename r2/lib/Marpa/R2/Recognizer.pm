16a448ee83f730f349ea3df7cf3832bfc889a4fb
- <->
2012-09-12 20:46:24 -0700

    Bumped version to indexed release 2.020000

--- r2/lib/Marpa/R2/Recognizer.pm
+++ r2/lib/Marpa/R2/Recognizer.pm
@@ -22,7 +22,7 @@ use integer;
 use English qw( -no_match_vars );
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.019_003';
+$VERSION        = '2.020000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
