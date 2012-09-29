b0937357fb41b0881cff77b39c07eafde39c4fa5
- <->
2012-09-28 19:20:31 -0700

    Bumped version # to 2.021_001

--- r2/lib/Marpa/R2/Grammar.pm
+++ r2/lib/Marpa/R2/Grammar.pm
@@ -32,7 +32,7 @@ use integer;
 use utf8;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.021_000';
+$VERSION        = '2.021_001';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
