5a46c7ebf01836a133363032315db49edaa874f1
- <->
2012-10-10 08:59:28 -0700

    Work on blog post

--- blog/html_config/contents.sh
+++ blog/html_config/contents.sh
@@ -1,14 +1,9 @@
-BASE=$HOME/Desktop/stage/Marpa--R2/
-for config in block-mixed block-inline block-pcdata
+BASE=../../r2/
+for config in inline-inline block-empty block-pcdata block-inline block-mixed
 do
 echo === $config ===
 echo '<marpa>-during-<span>-more inline stuff-<p>-new block-' |
-perl -I$BASE/$config/lib/perl5 $BASE/$config/bin/marpa_r2_html_fmt |
-perl -I$BASE/block-inline/lib/perl5 -MMarpa::R2::HTML=html \
-  -E '
-    local $/=undef;
-    my $text = ${html(\<STDIN>, { ":COMMENT" => sub {q{}} })};
-    $text =~ s/^\s*\n//gxms;
-    say $text
-  '  | tee $config.html
+  perl -I$BASE/html/lib -I$BASE/lib -I$BASE/blib/arch $BASE/html/script/marpa_r2_html_fmt \
+      --no-added-tag --compile g-$config.cfg |
+  tee $config.html
 done
