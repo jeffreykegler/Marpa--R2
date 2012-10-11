679de601f64f8ed2cf96444819a7203e610a412b
- <->
2012-10-10 18:30:33 -0700

    Work on blog post

--- blog/html_config/body.sh
+++
@@ -1,9 +0,0 @@
-BASE=../../r2/
-for config in pcdata inline mixed block
-do
-echo === $config ===
-echo 'before tag<p>after tag' |
-  perl -I$BASE/html/lib -I$BASE/lib -I$BASE/blib/arch $BASE/html/script/marpa_r2_html_fmt \
-      --no-added-tag --compile g-body-$config.cfg |
-  tee body-$config.html
-done
