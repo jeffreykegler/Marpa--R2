13cd497d89d8463f43722b034cf082f170b30a33
- <->
2012-10-10 22:45:18 -0700

    Work on blog post

--- blog/html_config/acme.sh
+++ blog/html_config/acme.sh
@@ -1,9 +1,11 @@
-BASE=../../r2/
-for config in inline-inline block-empty block-pcdata block-inline block-mixed
+for context in inline block
+do for content in empty pcdata inline mixed block
 do
-echo === $config ===
-echo '<marpa>-during-<span>-more inline stuff-<p>-new block-' |
-  perl -I$BASE/html/lib -I$BASE/lib -I$BASE/blib/arch $BASE/html/script/marpa_r2_html_fmt \
-      --no-added-tag --compile g-$config.cfg |
-  tee $config.html
+cat <<EO_SH > acme-$context-$content.sh
+cp default.cfg test.cfg
+echo "ELE_acme is a FLO_$content included in GRP_$context" >> test.cfg
+echo '<acme>-during-<span>-more inline stuff-<p>-new block-' |
+  marpa_r2_html_fmt --no-added-tag --compile test.cfg
+EO_SH
+done
 done
