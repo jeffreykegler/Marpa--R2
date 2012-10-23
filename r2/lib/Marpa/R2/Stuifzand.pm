ca1e2700850d51604890371b4e2eb26548a5421c
- <->
2012-10-23 15:21:20 -0700

    Bumped version # to 2.023_002

--- r2/lib/Marpa/R2/Stuifzand.pm
+++ r2/lib/Marpa/R2/Stuifzand.pm
@@ -23,7 +23,7 @@ use integer;
 use utf8;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.023_001';
+$VERSION        = '2.023_002';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
