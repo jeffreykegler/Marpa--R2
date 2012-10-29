ac615f0e758830f51d0e92ba14ad54a783952ba5
- <->
2012-10-28 19:34:06 -0700

    Work on new blog post

--- blog/error/error.pl
+++ blog/error/error.pl
@@ -14,16 +14,14 @@
 # General Public License along with Marpa::R2.  If not, see
 # http://www.gnu.org/licenses/.
 
-# Synopsis for Stuizand interface
-
 use 5.010;
 use strict;
 use warnings;
 use English qw( -no_match_vars );
 use Marpa::R2;
 
-my $grammar = Marpa::R2::Grammar->new(
-    {   start          => 'Script', 
+my $prefix_grammar = Marpa::R2::Grammar->new(
+    {   start          => 'Script',
         actions        => 'My_Actions',
         default_action => 'do_arg0',
         rules          => [ <<'END_OF_RULES' ]
@@ -35,25 +33,28 @@ END_OF_RULES
     }
 );
 
-sub My_Actions::do_add       { shift; return $_[1] + $_[2] }
+sub My_Actions::do_add  { shift; return $_[1] + $_[2] }
 sub My_Actions::do_arg0 { shift; return shift; }
 sub My_Actions::do_arg1 { shift; return $_[1]; }
 
-$grammar->precompute();
+$prefix_grammar->precompute();
 
 my @terminals = (
-    [ Number    => qr/\d+/xms,    "Number" ],
-    [ op_add    => qr/[+]/xms,    'Addition operator' ],
-    [ kw_say    => qr/say\b/xms,    '"say" keyword' ],
+    [ Number => qr/\d+/xms,   'Number' ],
+    [ op_add => qr/[+]/xms,   'Addition operator' ],
+    [ kw_say => qr/say\b/xms, '"say" keyword' ],
 );
 
 sub My_Error::last_completed_range {
     my ( $self, $symbol_name ) = @_;
-    my $grammar = $self->{grammar};
-    my $recce   = $self->{recce};
-    my @sought_rules =
-        grep { ($grammar->rule($_))[0] eq $symbol_name }
-        $grammar->rule_ids();
+    my $grammar      = $self->{grammar};
+    my $recce        = $self->{recce};
+    my @sought_rules = ();
+    for my $rule_id ( $grammar->rule_ids() ) {
+        my ($lhs) = $grammar->rule($rule_id);
+        $lhs =~ s/\[prec\d+\]\z//xms;
+        push @sought_rules, $rule_id if $lhs eq $symbol_name;
+    }
     die "Looking for completion of non-existent rule lhs: $symbol_name"
         if not scalar @sought_rules;
     my $latest_earley_set = $recce->latest_earley_set();
@@ -75,7 +76,7 @@ sub My_Error::last_completed_range {
     } ## end EARLEY_SET: while ( $earley_set >= 0 )
     return if $earley_set < 0;
     return ( $first_origin, $earley_set );
-} ## end sub last_completed_range
+} ## end sub My_Error::last_completed_range
 
 # Given a string, an earley set to position mapping,
 # and two earley sets, return the slice of the string
@@ -95,7 +96,7 @@ sub My_Error::show_last_expression {
     return
         defined $last_expression
         ? "Last expression successfully parsed was: $last_expression"
-        : "No expression was successfully parsed";
+        : 'No expression was successfully parsed';
 } ## end sub My_Error::show_last_expression
 
 sub My_Error::show_position {
@@ -104,12 +105,12 @@ sub My_Error::show_position {
     my $local_string = substr ${$input}, $position, 40;
     $local_string =~ s/\n/\\n/gxms;
     return $local_string;
-}
+} ## end sub My_Error::show_position
 
 sub my_parser {
     my ( $grammar, $string ) = @_;
     my @positions = (0);
-    my $recce         = Marpa::R2::Recognizer->new( { grammar => $grammar } );
+    my $recce = Marpa::R2::Recognizer->new( { grammar => $grammar } );
 
     # A quasi-object, for internal use only
     my $self = bless {
@@ -117,16 +118,19 @@ sub my_parser {
         input     => \$string,
         recce     => $recce,
         positions => \@positions
-        }, "My_Error";
+        },
+        'My_Error';
 
-    my $length        = length $string;
+    my $length = length $string;
     pos $string = $positions[-1];
     TOKEN: while ( pos $string < $length ) {
         next TOKEN if $string =~ m/\G\s+/gcxms;    # skip whitespace
         if ( $recce->exhausted() ) {
             die $self->show_last_expression(), "\n",
-            qq{The parse became exhausted here: "}, $self->show_position($positions[-1]), qq{"\n},
-        }
+                q{The parse became exhausted here: "},
+                $self->show_position( $positions[-1] ), qq{"\n},
+                ;
+        } ## end if ( $recce->exhausted() )
         TOKEN_TYPE: for my $t (@terminals) {
             my ( $token_name, $regex, $long_name ) = @{$t};
             next TOKEN_TYPE if not $string =~ m/\G($regex)/gcxms;
@@ -136,12 +140,13 @@ sub my_parser {
                 next TOKEN;
             }
             die $self->show_last_expression(), "\n",
-            "A problem occurred here: ", $self->show_position($positions[-1]), "\n",
+                'A problem occurred here: ',
+                $self->show_position( $positions[-1] ), "\n",
                 qq{Parser rejected token "$long_name"};
         } ## end TOKEN_TYPE: for my $t (@terminals)
-        die "A problem occurred here: ",
+        die 'A problem occurred here: ',
             $self->show_position( $positions[-1] ), "\n",
-            qq{No valid token was found};
+            q{No valid token was found};
     } ## end TOKEN: while ( pos $string < $length )
     my $value_ref = $recce->value;
     if ( not defined $value_ref ) {
@@ -151,8 +156,9 @@ sub my_parser {
     return ${$value_ref};
 } ## end sub my_parser
 
-TEST: for my $test_string (
-    '+ 1 2 3 + + 1 2 4',
+TEST:
+for my $test_string (
+    '+++ 1 2 3 + + 1 2 4',
     'say + 1 2',
     '+ 1 say 2',
     '+ 1 2 3 + + 1 2 4',
@@ -162,7 +168,8 @@ TEST: for my $test_string (
     )
 {
     my $output;
-    my $eval_ok = eval { $output = my_parser( $grammar, $test_string ); 1 };
+    my $eval_ok =
+        eval { $output = my_parser( $prefix_grammar, $test_string ); 1 };
     my $eval_error = $EVAL_ERROR;
     if ( not defined $eval_ok ) {
         chomp $eval_error;
@@ -175,11 +182,6 @@ TEST: for my $test_string (
     say q{=} x 30;
     print qq{Input was "$test_string"\n},
         qq{Parse was successful, output was "$output"\n};
-}
+} ## end TEST: for my $test_string ( '+++ 1 2 3 + + 1 2 4', 'say + 1 2'...)
 
-# Local Variables:
-#   mode: cperl
-#   cperl-indent-level: 4
-#   fill-column: 100
-# End:
 # vim: expandtab shiftwidth=4:
