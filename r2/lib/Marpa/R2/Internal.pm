bfc18657eed3efc41b130505e3cbf475ebca19a6
- <->
2012-11-15 20:45:46 -0800

    Bumped version to 2.025_003

--- r2/lib/Marpa/R2/Internal.pm
+++ r2/lib/Marpa/R2/Internal.pm
@@ -21,7 +21,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.025_002';
+$VERSION        = '2.025_003';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- r2/lib/Marpa/R2/Thin/Trace.pm
+++ r2/lib/Marpa/R2/Thin/Trace.pm
@@ -20,7 +20,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.025_002';
+$VERSION        = '2.025_003';
 $STRING_VERSION = $VERSION;
 $VERSION        = eval $VERSION;
 
