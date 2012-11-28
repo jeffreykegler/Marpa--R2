e1a8da5e5006e749d8a479efd9863bf981c8952c
- <->
2012-11-27 19:36:10 -0800

    Bumped version to 2.027_002

--- r2/html/lib/Marpa/R2/HTML/Callback.pm
+++ r2/html/lib/Marpa/R2/HTML/Callback.pm
@@ -20,7 +20,7 @@ use strict;
 package Marpa::R2::HTML::Callback;
 
 use vars qw( $VERSION $STRING_VERSION );
-$VERSION = '2.027_001';
+$VERSION = '2.027_002';
 $STRING_VERSION = $VERSION;
 ## use critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
