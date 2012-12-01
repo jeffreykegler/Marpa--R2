6d54c14fc6fe7ef8f9828875428c844cfe75d1df
- <->
2012-11-30 22:45:38 -0800

    Work on blog post

--- blog/sl/dsl1.pl
+++ blog/sl/dsl1.pl
@@ -9,32 +9,29 @@ use Marpa::R2 2.027_003;
 
 use Data::Dumper;
 
-sub add_brackets {
-    my ( undef, @children ) = @_;
-    return $children[0] if 1 == scalar @children;
-    my $original = join q{}, grep {defined} @children;
-    return '[' . $original . ']';
-} ## end sub add_brackets
-
 my $grammar = Marpa::R2::Grammar->new(
     {   scannerless => 1,
-        actions        => __PACKAGE__,
-        default_action => 'add_brackets',
+        action_object        => 'My_Actions',
+        default_action => 'do_what_I_mean',
         rules          => <<'END_OF_GRAMMAR',
 :start ::= expression
 expression ::=
      NUM
    | VAR
-   | '(' expression ')' assoc => group
-  || '-' expression
-  || expression '^' expression assoc => right
-  || expression '*' expression
-   | expression '/' expression
-  || expression '+' expression
-   | expression '-' expression
-  || VAR '=' expression
-NUM ~ [\d]+
-VAR ~ [\w]+
+   | '(' expression ')' assoc => group action => do_parens
+  || '-' expression action => do_uminus
+  || expression '**' expression
+    assoc => right action => do_power
+  || expression '*' expression action => do_multiply
+   | expression '/' expression action => do_divide
+  || expression '+' expression action => do_add
+   | expression '-' expression action => do_subtract
+  || expression '&' expression action => do_bitand
+  || expression '^' expression action => do_bitxor
+   | expression '|' expression action => do_bitor
+  || VAR '=' expression action => do_assign
+NUM ~ [\d]+ action => do_literal
+VAR ~ [\w]+ action => do_literal
 END_OF_GRAMMAR
     }
 );
@@ -44,6 +41,14 @@ sub calculate {
     my ($string) = @_;
     my $recce = Marpa::R2::Recognizer->new( { grammar => $grammar } );
 
+    ## A quasi-object, for internal use only
+    my $self = bless {
+        grammar => $grammar,
+        recce   => $recce,
+        },
+        'My_Actions';
+
+    local $My_Actions::SELF = $self;
     $recce->sl_read($string);
     my $value_ref = $recce->value;
 
@@ -65,18 +70,55 @@ my $output = join q{},
     report_calculation('4 * 3 / (a = b = 5) + 42 - 1'),
     report_calculation('4 * 3 /  5 - - - 3 + 42 - 1'),
     report_calculation('- a - b'),
-    report_calculation('1 * 2 + 3 * 4 ^ 2 ^ 2 ^ 2 * 42 + 1');
+    report_calculation('1 * 2 + 3 * 4 ** 2 ** 2 ** 2 * 42 + 1');
 
 print $output or die "print failed: $ERRNO";
 $output eq <<'EXPECTED_OUTPUT' or die 'FAIL: Output mismatch';
 Input: "4 * 3 + 42 / 1"
   Parse: [[4*3]+[42/1]]
 Input: "4 * 3 / (a = b = 5) + 42 - 1"
-  Parse: [[[[4*3]/[([a=[b=5]])]]+42]-1]
+  Parse: [[[[4*3]/([a=[b=5]])]+42]-1]
 Input: "4 * 3 /  5 - - - 3 + 42 - 1"
   Parse: [[[[[4*3]/5]-[-[-3]]]+42]-1]
 Input: "- a - b"
   Parse: [[-a]-b]
-Input: "1 * 2 + 3 * 4 ^ 2 ^ 2 ^ 2 * 42 + 1"
-  Parse: [[[1*2]+[[3*[4^[2^[2^2]]]]*42]]+1]
+Input: "1 * 2 + 3 * 4 ** 2 ** 2 ** 2 * 42 + 1"
+  Parse: [[[1*2]+[[3*[4**[2**[2**2]]]]*42]]+1]
 EXPECTED_OUTPUT
+
+package My_Actions;
+our $SELF;
+sub new { return $SELF }
+
+sub do_what_I_mean {
+    my $self = shift;
+    my @children = grep { defined } @_;
+    if (not scalar @children) {
+      return $self->do_literal();
+    }
+    return $children[0] if scalar @children == 1;
+    return \@children;
+} ## end sub add_brackets
+
+sub do_parens { shift; return '(' . $_[0] . ')' }
+sub do_power { shift; return '[' . $_[0] . '**' . $_[1] . ']' };
+sub do_multiply { shift; return '[' . $_[0] . '*' . $_[1] . ']' };
+sub do_divide { shift; return '[' . $_[0] . '/' . $_[1] . ']' };
+sub do_add { shift; return '[' . $_[0] . '+' . $_[1] . ']' };
+sub do_subtract { shift; return '[' . $_[0] . '-' . $_[1] . ']' };
+sub do_bitand { shift; return '[' . $_[0] . '&' . $_[1] . ']' };
+sub do_bitxor { shift; return '[' . $_[0] . '^' . $_[1] . ']' };
+sub do_bitor { shift; return '[' . $_[0] . '|' . $_[1] . ']' };
+sub do_uminus { shift; return '[-' . $_[0] . ']' };
+sub do_assign { shift; return '[' . $_[0] . '=' . $_[1] . ']' };
+
+sub do_literal {
+    my $self  = shift;
+    my $recce = $self->{recce};
+    my ( $start, $end ) = Marpa::R2::Context::location();
+    my $literal = $recce->sl_range_to_string( $start, $end );
+    $literal =~ s/ \s+ \z //xms;
+    $literal =~ s/ \A \s+ //xms;
+    return $literal;
+} ## end sub do_literal
+
