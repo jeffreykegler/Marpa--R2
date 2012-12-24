cd1210d8ff39b3f9a6d653d929004c28a4e2548a
- <->
2012-12-24 13:20:13 -0800

    Bumped version to 2.033_004: t+

--- r2/lib/Marpa/R2/Internal.pm
+++ r2/lib/Marpa/R2/Internal.pm
@@ -21,7 +21,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.033_003';
+$VERSION        = '2.033_004';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
