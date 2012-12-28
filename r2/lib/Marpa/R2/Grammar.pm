bba0a044b176f243ef149780a792145b74b48567
- <->
2012-12-27 19:00:15 -0800

    Bumped version to 2.035_001

--- r2/lib/Marpa/R2/Grammar.pm
+++ r2/lib/Marpa/R2/Grammar.pm
@@ -28,7 +28,7 @@ no warnings qw(recursion qw);
 use strict;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.035_000';
+$VERSION        = '2.035_001';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
