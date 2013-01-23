2874cc08e4b5bfa92a60eba35279b6d4bb24ec7c
Jeffrey Kegler <jkegl@cpan.org>
2013-01-22 20:48:25 -0800

    Bumped version to 2.043_000

--- r2/lib/Marpa/R2/Recognizer.pm
+++ r2/lib/Marpa/R2/Recognizer.pm
@@ -21,7 +21,7 @@ use strict;
 use English qw( -no_match_vars );
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.041_001';
+$VERSION        = '2.043_000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
