bad613e741ec8516dc7219377e832501b4992f23
Jeffrey Kegler <jkegl@cpan.org>
2013-02-02 19:38:00 -0800

    Speedup of Scanless interface: t+

--- blog/json/nyt.pl
+++ blog/json/nyt.pl
@@ -1,12 +1,13 @@
 use 5.010;
 
 use lib '.';
-use lib '.';
 use lib '../../r2/lib';
 use lib '../../r2/blib/arch';
-use Marpa::R2 2.043_002;
+use Marpa::R2;
 use MarpaX::JSON;
 use Data::Dumper;
+
+say $Marpa::R2::VERSION;
 
 local $/ = undef;
 my $json_str = <STDIN>;
