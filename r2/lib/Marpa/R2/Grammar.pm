091bc24735998569a0931e62716b422b580068ef
Jeffrey Kegler <jkegl@cpan.org>
2013-02-02 21:01:08 -0800

    Bumped version to 2.045_002

--- r2/lib/Marpa/R2/Grammar.pm
+++ r2/lib/Marpa/R2/Grammar.pm
@@ -28,7 +28,7 @@ no warnings qw(recursion qw);
 use strict;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.045_001';
+$VERSION        = '2.045_002';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
