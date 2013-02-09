85110a8b2987f39a15928f5605b59dddef9448c1
Jeffrey Kegler <jkegl@cpan.org>
2013-02-09 12:37:02 -0800

    Bump version to 2.047_001

--- r2/lib/Marpa/R2.pm
+++ r2/lib/Marpa/R2.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '2.047_000';
+$VERSION        = '2.047_001';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
