37e38e7b46143a48a42a331a0c697fcd64122c90
Jeffrey Kegler <jkegl@cpan.org>
2013-02-15 09:53:44 -0800

    Work on AST: t+

--- r2/lib/Marpa/R2/meta/try.pl
+++ r2/lib/Marpa/R2/meta/try.pl
@@ -47,20 +47,20 @@ my $parse_result =
     Marpa::R2::Scanless::G->_source_to_ast( $bnf );
 die "_source_to_ast did not return an AST" if not ref $parse_result eq 'REF';
 my $self = {};
-say "ast result = \n", Data::Dumper::Dumper(evaluate($self, ${$parse_result}));
+say "ast result = \n", Data::Dumper::Dumper(evaluate(${$parse_result}, $self));
 say "self object = \n", Data::Dumper::Dumper($self);
 
 exit 0;
 
 sub evaluate {
-    my ($self, $value) = @_;
+    my ($value, $self) = @_;
     return $value if not defined $value;
     if (Scalar::Util::blessed($value)) {
 	return $value->evaluate($self) if $value->can('evaluate');
-	return bless [ map { evaluate($self, $_) } @{$value} ], ref $value if Scalar::Util::reftype($value) eq 'ARRAY';
+	return bless [ map { evaluate($_, $self) } @{$value} ], ref $value if Scalar::Util::reftype($value) eq 'ARRAY';
 	return $value;
     }
-    return [ map { evaluate($self, $_) } @{$value} ] if ref $value eq 'ARRAY';
+    return [ map { evaluate($_, $self) } @{$value} ] if ref $value eq 'ARRAY';
     return $value;
 } ## end sub evaluate
 
@@ -100,13 +100,72 @@ exit 0;
 
 package Marpa::R2::Internal::MetaG::Symbol;
 
+use English qw( -no_match_vars );
+
 # Make the child argument into a symbol, if it is
 # not one already
 sub evaluate { return $_[0] };
 sub new {
-    my ( $class, $self ) = @_;
-    return bless { name => '' . $self }, $class if ref $self eq q{};
+    my ( $class, $self, $hide ) = @_;
+    return bless { name => ('' . $self), is_hidden => ($hide//0) }, $class if ref $self eq q{};
     return $self;
+}
+
+sub create_internal_symbol {
+    my ($self, $symbol_name) = @_;
+    $self->{needs_symbol}->{$symbol_name} = 1;
+    my $symbol = Marpa::R2::Internal::MetaG::Symbol->new($symbol_name);
+    return $symbol;
+}
+
+# Return the character class symbol name,
+# after ensuring everything is set up properly
+sub assign_symbol_by_char_class {
+    my ( $self, $char_class, $symbol_name ) = @_;
+
+    # default symbol name always start with TWO left square brackets
+    $symbol_name //= '[' . $char_class . ']';
+    $self->{character_classes} //= {};
+    my $cc_hash    = $self->{character_classes};
+    my (undef, $symbol) = $cc_hash->{$symbol_name};
+    if ( not defined $symbol ) {
+        my $regex;
+        if ( not defined eval { $regex = qr/$char_class/xms; 1; } ) {
+            Carp::croak( 'Bad Character class: ',
+                $char_class, "\n", 'Perl said ', $EVAL_ERROR );
+        }
+        $symbol = create_internal_symbol($self, $symbol_name);
+        $cc_hash->{$symbol_name} = [ $regex, $symbol ];
+    } ## end if ( not defined $hash_entry )
+    return $symbol;
+} ## end sub assign_symbol_by_char_class
+
+sub is_symbol { return 1 };
+sub name { return $_[0]->{name} }
+sub names { return $_[0]->{name} }
+sub is_hidden { return $_[0]->{is_hidden} }
+sub are_all_hidden { return $_[0]->{is_hidden} }
+
+sub is_lexical { return shift->{is_lexical} // 0 }
+sub hidden_set { return shift->{is_hidden} = 1; }
+sub lexical_set { return shift->{is_lexical} = 1; }
+sub mask { return shift->is_hidden() ? 0 : 1 }
+
+sub symbols { return $_[0]; }
+sub symbol_lists { return $_[0]; }
+
+package Marpa::R2::Internal::MetaG_Nodes::single_symbol;
+
+package Marpa::R2::Internal::MetaG_Nodes::kwc_ws_star;
+sub evaluate { return create_internal_symbol($_[1], $_[0]->[0]) }
+package Marpa::R2::Internal::MetaG_Nodes::kwc_ws_plus;
+sub evaluate { return create_internal_symbol($_[1], $_[0]->[0]) }
+package Marpa::R2::Internal::MetaG_Nodes::kwc_ws;
+sub evaluate { return create_internal_symbol($_[1], $_[0]->[0]) }
+package Marpa::R2::Internal::MetaG_Nodes::kwc_any;
+sub evaluate {
+    return Marpa::R2::Internal::MetaG::Symbol::assign_symbol_by_char_class(
+        $_[1], '[\p{Cn}\P{Cn}]', $_[0]->[0] );
 }
 
 package Marpa::R2::Internal::MetaG_Nodes::single_symbol;
@@ -117,7 +176,15 @@ package Marpa::R2::Internal::MetaG_Nodes::symbol_name;
 sub evaluate { return $_[0]->[0]->evaluate(); }
 
 package Marpa::R2::Internal::MetaG_Nodes::character_class;
-sub evaluate { return Marpa::R2::Internal::MetaG::Symbol->new( $_[0]->[0] ); }
+
+sub evaluate {
+    my ( $values, $self ) = @_;
+    my $symbol =
+        Marpa::R2::Internal::MetaG::Symbol::assign_symbol_by_char_class(
+        $self, $values->[0] );
+    $symbol->lexical_set();
+    return $symbol;
+} ## end sub evaluate
 
 package Marpa::R2::Internal::MetaG_Nodes::bare_name;
 sub evaluate { return Marpa::R2::Internal::MetaG::Symbol->new( $_[0]->[0] ); }
