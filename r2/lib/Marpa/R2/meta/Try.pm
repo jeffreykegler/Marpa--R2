21161c4512841f713dc75fd1388eeeb90838ba26
Jeffrey Kegler <jkegl@cpan.org>
2013-02-26 21:58:45 -0800

    Working on AST: t+

--- r2/lib/Marpa/R2/meta/Try.pm
+++ r2/lib/Marpa/R2/meta/Try.pm
@@ -13,82 +13,13 @@
 # General Public License along with Marpa::R2.  If not, see
 # http://www.gnu.org/licenses/.
 
-package Marpa::R2::Scanless;
+package Marpa::R2::Internal::MetaAST;
 
 use 5.010;
 use strict;
 use warnings;
 
 say STDERR "Trial ", __FILE__;
-
-my %hashed_closures = (
-    do_adverb_list               => \&do_adverb_list,
-    do_any                       => \&do_any,
-    do_character_class           => \&do_character_class,
-    do_discard_rule              => \&do_discard_rule,
-    do_empty_rule                => \&do_empty_rule,
-    do_lhs                       => \&do_lhs,
-    do_op_declare_bnf            => \&do_op_declare_bnf,
-    do_op_declare_match          => \&do_op_declare_match,
-    do_op_plus_quantifier        => \&do_op_plus_quantifier,
-    do_op_star_quantifier        => \&do_op_star_quantifier,
-    do_parenthesized_rhs_primary_list => \&do_parenthesized_rhs_primary_list,
-    do_priority_rule             => \&do_priority_rule,
-    do_quantified_rule           => \&do_quantified_rule,
-    do_rhs                       => \&do_rhs,
-    do_rules                     => \&do_rules,
-    do_separator_specification   => \&do_separator_specification,
-    do_single_quoted_string      => \&do_single_quoted_string,
-    do_start_rule                => \&do_start_rule,
-    do_symbol                    => \&do_symbol,
-    do_rhs_primary_list               => \&do_rhs_primary_list,
-    do_ws                        => \&do_ws,
-    do_ws_plus                   => \&do_ws_plus,
-    do_ws_star                   => \&do_ws_star,
-);
-
-# Applied first.  If a rule has only one RHS symbol,
-# and the key is it, the value is the action.
-my %actions_by_rhs_symbol = (
-    'kwc ws star'          => 'do_ws_star',
-    'kwc ws plus'          => 'do_ws_plus',
-    'kwc ws'               => 'do_ws',
-    'kwc any'              => 'do_any',
-    'single quoted string' => 'do_single_quoted_string',
-    'character class'      => 'do_character_class',
-    'bracketed name'       => 'do_bracketed_name',
-    'op star'              => 'do_op_star_quantifier',
-    'op plus'              => 'do_op_plus_quantifier',
-    'op declare bnf'       => 'do_op_declare_bnf',
-    'op declare match'     => 'do_op_declare_match',
-);
-
-# Applied second.  Use the LHS symbol to
-# determine the action
-my %actions_by_lhs_symbol = (
-    symbol                           => 'do_symbol',
-    rhs                              => 'do_rhs',
-    lhs                              => 'do_lhs',
-    'rhs primary list'               => 'do_rhs_primary_list',
-    'parenthesized rhs primary list' => 'do_parenthesized_rhs_primary_list',
-    rules                            => 'do_rules',
-    'start rule'                     => 'do_start_rule',
-    'priority rule'                  => 'do_priority_rule',
-    'empty rule'                     => 'do_empty_rule',
-    'quantified rule'                => 'do_quantified_rule',
-    'discard rule'                   => 'do_discard_rule',
-    priorities                       => 'do_discard_separators',
-    alternatives                     => 'do_discard_separators',
-    alternative                      => 'do_alternative',
-    'adverb list'                    => 'do_adverb_list',
-    action                           => 'do_action',
-    blessing                         => 'do_blessing',
-    'left association'               => 'do_left_association',
-    'right association'              => 'do_right_association',
-    'group association'              => 'do_group_association',
-    'separator specification'        => 'do_separator_specification',
-    'proper specification'           => 'do_proper_specification',
-);
 
 sub Marpa::R2::Scanless::G::_source_to_ast {
     my ( $self, $p_rules_source ) = @_;
