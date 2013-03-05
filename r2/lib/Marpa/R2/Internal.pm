4d80d3aa3c8846c29ae7a07e940f3b0cc709fb07
Jeffrey Kegler <jkegl@cpan.org>
2013-03-04 17:46:32 -0800

    Bump version to 2.047_010

--- r2/lib/Marpa/R2/Internal.pm
+++ r2/lib/Marpa/R2/Internal.pm
@@ -21,7 +21,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.047_009';
+$VERSION        = '2.047_010';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- r2/lib/Marpa/R2/Thin/Trace.pm
+++ r2/lib/Marpa/R2/Thin/Trace.pm
@@ -20,7 +20,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.047_009';
+$VERSION        = '2.047_010';
 $STRING_VERSION = $VERSION;
 $VERSION        = eval $VERSION;
 
