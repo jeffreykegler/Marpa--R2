5bb8e174fdd1f20626375b623e081e2be97a5ea5
Jeffrey Kegler <jkegl@cpan.org>
2013-04-05 21:31:07 -0700

    Bumped version to 2.051_005

--- r2/lib/Marpa/R2/Internal.pm
+++ r2/lib/Marpa/R2/Internal.pm
@@ -21,7 +21,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.051_004';
+$VERSION        = '2.051_005';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
