b235a3ecf62c320642c195b13b506d7f4f64056f
Jeffrey Kegler <jkegl@cpan.org>
2013-05-08 10:10:23 -0700

    Bumped version to 2.053_007

--- cpan/lib/Marpa/R2/Internal.pm
+++ cpan/lib/Marpa/R2/Internal.pm
@@ -21,7 +21,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.053_006';
+$VERSION        = '2.053_007';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- cpan/lib/Marpa/R2/MetaAST.pm
+++ cpan/lib/Marpa/R2/MetaAST.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.053_006';
+$VERSION        = '2.053_007';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
