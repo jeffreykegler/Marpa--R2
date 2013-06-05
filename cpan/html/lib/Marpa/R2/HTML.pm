88d3d3b72d0a5b4302ca0482e44330cd530e332c
Jeffrey Kegler <jkegl@cpan.org>
2013-06-04 21:51:18 -0700

    Bump version to 2.057_002

--- cpan/html/lib/Marpa/R2/HTML.pm
+++ cpan/html/lib/Marpa/R2/HTML.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw( $VERSION $STRING_VERSION );
-$VERSION        = '2.057_001';
+$VERSION        = '2.057_002';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
