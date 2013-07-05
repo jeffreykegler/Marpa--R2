c00bab1570e5737a125741f3bfeecf6fd01580d8
Jeffrey Kegler <jkegl@cpan.org>
2013-07-05 08:45:58 -0700

    Bumped version to 2.063_001

--- cpan/lib/Marpa/R2.pm
+++ cpan/lib/Marpa/R2.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '2.063_000';
+$VERSION        = '2.063_001';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
