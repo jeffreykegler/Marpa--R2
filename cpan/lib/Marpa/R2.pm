5e192444950e60e8b032f7d864a7023ce466a094
Jeffrey Kegler <jkegl@cpan.org>
2013-07-30 16:30:19 -0700

    Bumped version to 2.065_005

--- cpan/lib/Marpa/R2.pm
+++ cpan/lib/Marpa/R2.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '2.065_004';
+$VERSION        = '2.065_005';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- cpan/lib/Marpa/R2/Thin/Trace.pm
+++ cpan/lib/Marpa/R2/Thin/Trace.pm
@@ -20,7 +20,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.065_004';
+$VERSION        = '2.065_005';
 $STRING_VERSION = $VERSION;
 $VERSION        = eval $VERSION;
 
