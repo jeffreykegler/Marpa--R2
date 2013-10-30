640a2e2dd5222854be3782448d72cd6c4479e9dd
Jeffrey Kegler <jkegl@cpan.org>
2013-10-29 20:29:16 -0700

    Creating version 2.074000

--- cpan/lib/Marpa/R2/Thin/Trace.pm
+++ cpan/lib/Marpa/R2/Thin/Trace.pm
@@ -20,7 +20,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.073_001';
+$VERSION        = '2.074000';
 $STRING_VERSION = $VERSION;
 $VERSION        = eval $VERSION;
 
