bf95d4e9eb1e3cc23bdb6ef1c6e1ef4bb4bfab71
Jeffrey Kegler <JKEGL@cpan.org>
2013-11-16 15:30:57 -0800

    Bumped version to 2.075_005

--- cpan/html/lib/Marpa/R2/HTML/Internal.pm
+++ cpan/html/lib/Marpa/R2/HTML/Internal.pm
@@ -24,7 +24,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.075_004';
+$VERSION        = '2.075_005';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
--- cpan/lib/Marpa/R2.pm
+++ cpan/lib/Marpa/R2.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '2.075_004';
+$VERSION        = '2.075_005';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
