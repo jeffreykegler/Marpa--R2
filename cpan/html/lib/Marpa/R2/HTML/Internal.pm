db2cd1d3523f6421e456c32299ddc0f3f9136aa9
Jeffrey Kegler <JKEGL@cpan.org>
2013-12-21 19:31:13 -0800

    Bumped version to 2.077_014

--- cpan/html/lib/Marpa/R2/HTML/Internal.pm
+++ cpan/html/lib/Marpa/R2/HTML/Internal.pm
@@ -24,7 +24,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.077_013';
+$VERSION        = '2.077_014';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
--- cpan/lib/Marpa/R2.pm
+++ cpan/lib/Marpa/R2.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '2.077_013';
+$VERSION        = '2.077_014';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
