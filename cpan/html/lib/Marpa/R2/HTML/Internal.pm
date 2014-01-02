fde8293dcd2b0e1354cf9abaac41f4b1f5720d38
Jeffrey Kegler <JKEGL@cpan.org>
2014-01-01 20:25:21 -0800

    Bumped version to 2.079_002

--- cpan/html/lib/Marpa/R2/HTML/Internal.pm
+++ cpan/html/lib/Marpa/R2/HTML/Internal.pm
@@ -24,7 +24,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.079_001';
+$VERSION        = '2.079_002';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
--- cpan/meta/make_internal_pm.pl
+++ cpan/meta/make_internal_pm.pl
@@ -97,7 +97,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.079_001';
+$VERSION        = '2.079_002';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 END_OF_HEADER
