94e8cee65144d79fa0ae74d0dbf8b980e752143f
Jeffrey Kegler <JKEGL@cpan.org>
2014-01-04 17:58:31 -0800

    Touch-up valgrind files

--- cpan/etc/dovg.sh
+++ cpan/etc/dovg.sh
@@ -1,6 +1,8 @@
+# Intended to be run from the cpan directory
 ls t/*.t html/t/*.t |
 while read t
-do echo $t |
-  perl -Ilib -Iblib/arch \
-    -E 'my $t = <STDIN>; chomp $t; use Test::Valgrind (file => $t, extra_supps => ["my_suppressions"]);' 
-done
+do
+  echo === $t ===;
+  echo $t | perl -Ilib -Iblib/arch \
+    -E 'my $t = <STDIN>; chomp $t; use Test::Valgrind (file => $t, extra_supps => ["etc/my_suppressions"]);' 
+done 2>&1 | tee dovg.log
