4bf01395349c615830ac365087f095aa347b2a4a
Jeffrey Kegler <JKEGL@cpan.org>
2014-01-20 14:01:38 -0800

    Bump version to 2.079_011: t+

--- cpan/lib/Marpa/R2/Internal.pm
+++ cpan/lib/Marpa/R2/Internal.pm
@@ -24,7 +24,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.079_010';
+$VERSION        = '2.079_011';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
