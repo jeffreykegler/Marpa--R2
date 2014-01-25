d5b33f8b583546470d1450e4401ebedae45fe6ca
Jeffrey Kegler <JKEGL@cpan.org>
2014-01-24 21:13:19 -0800

    Gitub pull request #89 and #93: t+
    Hand-patch fix to merge conflict resolution between these two
    github issues.

--- cpan/lib/Marpa/R2/Value.pm
+++ cpan/lib/Marpa/R2/Value.pm
@@ -1147,10 +1147,12 @@ sub init_registrations {
 
             my @push_ops = ();
             my $array_descriptor = substr $semantics, 1, -1;
+            $array_descriptor =~ s/^\s*|\s*$//g;
             RESULT_DESCRIPTOR:
-            for my $result_descriptor ( split /[,]/xms,
+            for my $result_descriptor ( split /[,]\s*/xms,
                 $array_descriptor )
             {
+                $result_descriptor =~ s/^\s*|\s*$//g;
                 if ( $result_descriptor eq 'start' ) {
                     push @push_ops, $op_push_start_location;
                     next RESULT_DESCRIPTOR;
