296a6e8b0e1fb9af2e1b5730715522d2a35df169
Jeffrey Kegler <JKEGL@cpan.org>
2014-02-11 09:09:01 -0800

    Phase 4: t+

--- cpan/xs/gp_generate.pl
+++ cpan/xs/gp_generate.pl
@@ -49,6 +49,7 @@ if ( @ARGV == 1 ) {
 
 my %format_by_type = (
    int => '%d',
+   Marpa_Assertion_ID => '%d',
    Marpa_IRL_ID => '%d',
    Marpa_NSY_ID => '%d',
    Marpa_Or_Node_ID => '%d',
@@ -194,6 +195,8 @@ say {$out} gp_generate(qw(symbol_is_terminal_set Marpa_Symbol_ID symbol_id int b
 say {$out} gp_generate(qw(symbol_is_valued Marpa_Symbol_ID symbol_id));
 say {$out} gp_generate(qw(symbol_is_valued_set Marpa_Symbol_ID symbol_id int boolean));
 say {$out} gp_generate(qw(symbol_new));
+say {$out} gp_generate(qw(zwa_new int default_value));
+say {$out} gp_generate(qw(zwa_place Marpa_Assertion_ID zwaid Marpa_Rule_ID xrl_id int rhs_ix));
 
 $main::CLASS_LETTER   = 'r';
 $main::LIBMARPA_CLASS = 'Marpa_Recognizer';
