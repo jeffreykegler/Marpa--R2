ecef8f4f57c1a6204e0ecb5df1e0662dcc7a881a
rns <ruslan.shvedov@gmail.com>
2014-02-16 08:59:52 +0200

    sub name touchup

--- cpan/lib/Marpa/R2/Grammar.pm
+++ cpan/lib/Marpa/R2/Grammar.pm
@@ -1422,7 +1422,7 @@ sub add_user_rule {
         $grammar_c->throw_set(1);
 
         if ( $ordinary_rule_id < 0 ) {
-            my $rule_description = rule_description( $lhs_name, $rhs_names );
+            my $rule_description = describe_rule( $lhs_name, $rhs_names );
             my ( $error_code, $error_string ) = $grammar_c->error();
             $error_code //= -1;
             my $problem_description =
@@ -1487,7 +1487,7 @@ sub add_user_rule {
     );
     $grammar_c->throw_set(1);
     if ( not defined $original_rule_id or $original_rule_id < 0) {
-        my $rule_description = rule_description( $lhs_name, $rhs_names );
+        my $rule_description = describe_rule( $lhs_name, $rhs_names );
         my ( $error_code, $error_string ) = $grammar_c->error();
         $error_code //= -1;
         my $problem_description =
@@ -1526,12 +1526,12 @@ sub add_user_rule {
 
 } ## end sub add_user_rule
 
-sub rule_description {
+sub describe_rule {
     my ( $lhs_name, $rhs_names ) = @_;
     # wrap symbol names with whitespaces allowed by SLIF
     $lhs_name = "<$lhs_name>" if $lhs_name =~ / /;
     return "$lhs_name -> " . ( join q{ }, map { / / ? "<$_>" : $_ } @{$rhs_names} );
-} ## end sub rule_description
+} ## end sub describe_rule
 
 sub set_start_symbol {
     my $grammar = shift;
