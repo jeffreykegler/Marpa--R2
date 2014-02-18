88be93c2695c781f1f17a27c6d73242e7790064d
Jeffrey Kegler <JKEGL@cpan.org>
2014-02-17 17:16:13 -0800

    Bumped version to 2.080000

--- cpan/meta/make_internal_pm.pl
+++ cpan/meta/make_internal_pm.pl
@@ -97,7 +97,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.079_015';
+$VERSION        = '2.080000';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 END_OF_HEADER
