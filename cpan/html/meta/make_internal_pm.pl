e65ef96f01f34dd04f6439ac77dc9eeae8e6912e
Jeffrey Kegler <JKEGL@cpan.org>
2014-04-21 11:14:02 -0700

    Bump version #

--- cpan/html/meta/make_internal_pm.pl
+++ cpan/html/meta/make_internal_pm.pl
@@ -97,7 +97,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.085_002';
+$VERSION        = '2.085_003';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 END_OF_HEADER
