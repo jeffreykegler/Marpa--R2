c136c734a75af09cb8fa0c5824d74cbe97ca9ba9
Jeffrey Kegler <JKEGL@cpan.org>
2014-05-26 21:03:56 -0700

    Bump version: t+

--- cpan/lib/Marpa/R2/Internal.pm
+++ cpan/lib/Marpa/R2/Internal.pm
@@ -24,7 +24,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.085_006';
+$VERSION        = '2.085_007';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
