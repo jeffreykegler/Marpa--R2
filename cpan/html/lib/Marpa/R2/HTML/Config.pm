6ca1b2def8f6ceec77134150d42d4335e23ba1bd
Jeffrey Kegler <JKEGL@cpan.org>
2014-09-10 18:10:10 -0700

    Bump version to 2.093_000

--- cpan/html/lib/Marpa/R2/HTML/Config.pm
+++ cpan/html/lib/Marpa/R2/HTML/Config.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.091_000';
+$VERSION        = '2.093_000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- cpan/lib/Marpa/R2/ASF.pm
+++ cpan/lib/Marpa/R2/ASF.pm
@@ -21,7 +21,7 @@ use warnings;
 no warnings qw(recursion);
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.091_000';
+$VERSION        = '2.093_000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
