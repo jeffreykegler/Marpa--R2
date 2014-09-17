a9ae358d706641be4719ccaed35432c1ab882c8a
Jeffrey Kegler <JKEGL@cpan.org>
2014-09-16 19:00:43 -0700

    Bump version to 2.094000

--- cpan/lib/Marpa/R2/Recognizer.pm
+++ cpan/lib/Marpa/R2/Recognizer.pm
@@ -21,7 +21,7 @@ use strict;
 use English qw( -no_match_vars );
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.093_000';
+$VERSION        = '2.094000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- cpan/meta/make_internal_pm.pl
+++ cpan/meta/make_internal_pm.pl
@@ -97,7 +97,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.093_000';
+$VERSION        = '2.094000';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 END_OF_HEADER
