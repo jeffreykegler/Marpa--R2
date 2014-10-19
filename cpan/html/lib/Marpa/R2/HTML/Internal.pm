f4cb26bf942f27fc8ee2112f55d855531068e51f
Jeffrey Kegler <JKEGL@cpan.org>
2014-10-18 20:23:32 -0700

    Bump version to 2.097_002

--- cpan/html/lib/Marpa/R2/HTML/Internal.pm
+++ cpan/html/lib/Marpa/R2/HTML/Internal.pm
@@ -24,7 +24,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.097_001';
+$VERSION        = '2.097_002';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
--- cpan/meta/make_internal_pm.pl
+++ cpan/meta/make_internal_pm.pl
@@ -97,7 +97,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.097_001';
+$VERSION        = '2.097_002';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 END_OF_HEADER
