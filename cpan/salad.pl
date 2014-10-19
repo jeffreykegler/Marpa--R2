01036d9b059242a84a268934c092f0c025c1646d
Jeffrey Kegler <JKEGL@cpan.org>
2014-10-19 09:24:50 -0700

    Work on salad example

--- cpan/salad.pl
+++ cpan/salad.pl
@@ -3,11 +3,43 @@
 use 5.010;
 use strict;
 use warnings;
-use Marpa::R2;
-use File::Slurp 'read_file';
+use Marpa::R2 2.097_002;
 use Data::Dumper;
 
-$Data::Dumper::Deepcopy = 1;    # for better dumps
+# New (as yet undocumented) features:
+# 
+# 1.) The $recce->last_completed_span($symbol) method
+# returns the location of the most recent completion
+# of $symbol.  Locations are in input stream terms.
+# It works similarly to the documented method,
+# $recce->last_completed($symbol).
+#
+# 2.) A scanless recognizer setting: 'exhaustion'
+# If its value is "fatal",
+# when the SLIF tries to continue
+# reading input after exhaustion, it throws an
+# exception.  If "event", it returns an event
+# named "'exhausted".  Note the initial single quote,
+# which marks it as a reserved event.
+#
+# The 'exhaustion event only occurs when needed to
+# make the $recce->read() or $recce->resume() method
+# return.  Applications which want to check for exhaustion
+# should ignore the event and
+# use the $recce->exhausted() method.
+#
+# 3.) A new scanless recognizer setting: 'rejection'
+# It determines what happens when
+# all alternatives at a location are rejected.
+# If its value is 'fatal', an exception is thrown.
+# If its value is 'event', a "'rejection" event
+# occurs.  Note the initial single quote,
+# which marks it as a reserved event.
+#
+# 4.) Lexeme priorities can now be changed on the
+# fly, using the
+# $recce->lexeme_priority_set( $lexeme_name, $priority )
+# call.
 
 my $grammar = << '=== GRAMMAR ===';
 :default ::= action => [ name, value ]
@@ -26,7 +58,6 @@ balanced ::=
 
 contents ::= <content item>*
 <content item> ::= balanced | filler
-empty ::=
 
 <prefix lexeme> ~ <deep filler>
 filler ~ <deep filler>
@@ -62,17 +93,40 @@ rsquare ~ <deep rsquare>
 
 my $g = Marpa::R2::Scanless::G->new( { source => \($grammar) } );
 
-# my $recce_debug_args = { trace_terminals => 1, trace_values => 1 };
-my $recce_debug_args = {};
+# Test strings go here
+#             012345678901234567890
+my @strings = ( 'z}ab)({[]})))(([]))zz',
+'9\090]{[][][9]89]8[][]90]{[]\{}{}09[]}[',
+'([]([])([]([]',
+);
+
+for my $string (@strings) {
+    my $finds = test($g, $string);
+    say "Input: $string";
+    for ( my $i = 0; $i < scalar @{$finds}; $i++ ) {
+        say join " ", "Find", ( $i + 1 . ":" ), $finds->[$i];
+    }
+} ## end for my $string (@strings)
 
 sub test {
-    my ($string) = @_;
+    my ($g, $string) = @_;
     my @found = ();
     # say STDERR "Input: $string";
     my $input_length = length $string;
     my $target_start = 0;
 
+    # my $recce_debug_args = { trace_terminals => 1, trace_values => 1 };
+    state $recce_debug_args = {};
+
+    # One pass through this loop for every target found,
+    # until we reach end of string without finding a target
+
     TARGET: while ( $target_start < $input_length ) {
+
+        # First we find the "shortest span" -- the one which ends earliest.
+        # This tells us where the prefix should end.
+        # No prefix should go beyond the first location of the shortest span.
+
         my @shortest_span = ();
         my $recce         = Marpa::R2::Scanless::R->new(
             {   grammar    => $g,
@@ -99,7 +153,13 @@ sub test {
 
         last TARGET if not scalar @shortest_span;
 
-        # We end the prefix here
+        # We now have found the longest allowed prefix.
+        # Our "longest match" will begin at the end of this prefix,
+        # or before it.
+
+        # We just run until exhausted, the  look for the last
+        # completed <target>.  This will be our longest match.
+
         # say STDERR join q{ }, @shortest_span;
         my $prefix_end = $shortest_span[0];
         $recce = Marpa::R2::Scanless::R->new(
@@ -119,7 +179,10 @@ sub test {
 
         last TARGET if not scalar @longest_span;
         push @found, $recce->literal(@longest_span);
-        say "Found target at $pos: ", $recce->literal(@longest_span);
+        # say "Found target at $pos: ", $recce->literal(@longest_span);
+
+        # Move the search location forward,
+        # in preparation for looking for the next target
 
         $target_start = $longest_span[0] + $longest_span[1];
 
@@ -127,22 +190,4 @@ sub test {
     return \@found;
 } ## end sub test
 
-#             012345678901234567890
-my @strings = ( 'z}ab)({[]})))(([]))zz',
-'9\090]{[][][9]89]8[][]90]{[]\{}{}09[]}[',
-'([]([])([]([]',
-);
-
-for my $string (@strings) {
-    my $finds = test($string);
-    say "Input: $string";
-    for ( my $i = 0; $i < scalar @{$finds}; $i++ ) {
-        say join " ", "Find", ( $i + 1 . ":" ), $finds->[$i];
-    }
-} ## end for my $string (@strings)
-
-# my $ref_value = $recce->value();
-# die "No parse" if not $ref_value;
-# my $tree = ${$ref_value};
-# say Dumper($tree);
-
+# vim: expandtab shiftwidth=4:
