d100f05c2c102e9a3643c8caf71f98509af4a53c
Jeffrey Kegler <JKEGL@cpan.org>
2014-11-17 18:27:38 -0800

    Bump version to 2.101_000

--- cpan/lib/Marpa/R2/SLG.pm
+++ cpan/lib/Marpa/R2/SLG.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.100000';
+$VERSION        = '2.101_000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- cpan/lib/Marpa/R2/Thin/Trace.pm
+++ cpan/lib/Marpa/R2/Thin/Trace.pm
@@ -20,7 +20,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.100000';
+$VERSION        = '2.101_000';
 $STRING_VERSION = $VERSION;
 $VERSION        = eval $VERSION;
 
