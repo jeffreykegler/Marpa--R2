b26982befb8b4bdc81ea8c54e70119b33b61b267
Jeffrey Kegler <JKEGL@cpan.org>
2015-01-05 17:50:21 -0800

    Add license to new files

--- cpan/etc/pod_inlinks.pl
+++ cpan/etc/pod_inlinks.pl
@@ -1,3 +1,19 @@
+#!/usr/bin/perl
+# Copyright 2014 Jeffrey Kegler
+# This file is part of Marpa::R2.  Marpa::R2 is free software: you can
+# redistribute it and/or modify it under the terms of the GNU Lesser
+# General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Marpa::R2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser
+# General Public License along with Marpa::R2.  If not, see
+# http://www.gnu.org/licenses/.
+
 use 5.010;
 
 use Pod::Simple::SimpleTree;
