3beaf9a5f8331157e56d1ab9a10c72db6864c34f
Jeffrey Kegler <JKEGL@cpan.org>
2015-01-09 18:49:57 -0800

    Bump version to 2.103_000

--- cpan/lib/Marpa/R2/Recognizer.pm
+++ cpan/lib/Marpa/R2/Recognizer.pm
@@ -21,7 +21,7 @@ use strict;
 use English qw( -no_match_vars );
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.101_000';
+$VERSION        = '2.103_000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
