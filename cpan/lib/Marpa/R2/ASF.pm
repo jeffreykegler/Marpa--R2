5cefcddc201ae8e94754ff47a52f77551aabaa09
Jeffrey Kegler <JKEGL@cpan.org>
2015-01-09 17:05:32 -0800

    Bump version to 2.102000

--- cpan/lib/Marpa/R2/ASF.pm
+++ cpan/lib/Marpa/R2/ASF.pm
@@ -21,7 +21,7 @@ use warnings;
 no warnings qw(recursion);
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.101_000';
+$VERSION        = '2.102000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- cpan/lib/Marpa/R2/MetaAST.pm
+++ cpan/lib/Marpa/R2/MetaAST.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.101_000';
+$VERSION        = '2.102000';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
