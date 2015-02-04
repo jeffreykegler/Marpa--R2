3350a782cb9fdf0e197cd16144f7de339282c93d
Jeffrey Kegler <JKEGL@cpan.org>
2015-02-03 20:20:37 -0800

    Bump version to 2.103_004

--- cpan/lib/Marpa/R2/Value.pm
+++ cpan/lib/Marpa/R2/Value.pm
@@ -20,7 +20,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.103_003';
+$VERSION        = '2.103_004';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
