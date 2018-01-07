b5179cbea25f7bb45c28f297c4c29809af7c3c2a
Jeffrey Kegler <jeffreykegler@jeffreykegler.com>
2018-01-06 16:24:56 -0800

    Update copyright

--- cpan/t/sl_taint.t
+++ cpan/t/sl_taint.t
@@ -1,5 +1,5 @@
 #!perl -T
-# Copyright 2015 Jeffrey Kegler
+# Copyright 2018 Jeffrey Kegler
 # This file is part of Marpa::R2.  Marpa::R2 is free software: you can
 # redistribute it and/or modify it under the terms of the GNU Lesser
 # General Public License as published by the Free Software Foundation,
--- cpan/xs/create_ops.pl
+++ cpan/xs/create_ops.pl
@@ -1,5 +1,5 @@
 #!perl
-# Copyright 2015 Jeffrey Kegler
+# Copyright 2018 Jeffrey Kegler
 # This file is part of Marpa::R2.  Marpa::R2 is free software: you can
 # redistribute it and/or modify it under the terms of the GNU Lesser
 # General Public License as published by the Free Software Foundation,
@@ -57,7 +57,7 @@ my @ops = sort { $a->[0] cmp $b->[0] }
 
 say <<'END_OF_PREAMBLE';
 /*
- * Copyright 2015 Jeffrey Kegler
+ * Copyright 2018 Jeffrey Kegler
  * This file is part of Libmarpa.  Libmarpa is free software: you can
  * redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation,
